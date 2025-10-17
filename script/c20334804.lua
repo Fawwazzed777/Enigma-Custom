cdc5d0e625638a5652db75d2b2230e6346b1296f
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-10-17 21:21:20 +0700

    Fix

--- script/c20334804.lua
+++ script/c20334804.lua
@@ -63,15 +63,20 @@ end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsDestructable() end
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMacthingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,0,1,1,nil)
+	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirst()
-	if tc:IsRelateToEffect(e) then
-		Duel.HintSelection(tc)
-		Duel.Destroy(tc,REASON_EFFECT)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	local tc=g:GetFirst()
+	local atk=tc:GetTextAttack()
+	if tc then
+		Duel.HintSelection(g)
+		if Duel.Destroy(tc,REASON_EFFECT)~=0 and tc:IsOriginalType(TYPE_MONSTER) then
+		Duel.BreakEffect()
+		Duel.Damage(1-tp,atk/2,REASON_EFFECT)
+		end
 	end
 end
 --
