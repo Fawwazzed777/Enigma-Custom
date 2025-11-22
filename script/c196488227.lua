aa02deb74a37ba5663bf4a344fab323eb1f345de
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-22 13:37:17 +0700

    Updates

---
+++ script/c196488227.lua
@@ -0,0 +1,104 @@
+--Enigmation Force - Crest Keeper, Varniel
+local s,id=GetID()
+function s.initial_effect(c)
+Pendulum.AddProcedure(c)
+	--search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--effect gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(s.efcon)
+	e3:SetOperation(s.efop)
+	c:RegisterEffect(e3)
+	--To Bottom
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_PZONE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetTarget(s.tetg)
+	e4:SetOperation(s.teop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x303,0x344}
+function s.filter(c,code)
+	return c:IsFaceup() and c:IsCode(code)
+end
+function s.thfilter(c,tp)
+	return (c:IsSetCard(0x303) or c:IsSetCard(0x344)) and c:IsAbleToHand()
+		and not Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_ONFIELD,0,1,nil,c:GetCode())
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+end
+end
+function s.efcon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_FUSION+REASON_SYNCHRO+REASON_XYZ)~=0
+		and (p:GetReasonCard():IsAttribute(ATTRIBUTE_DARK) or p:GetReasonCard():IsAttribute(ATTRIBUTE_LIGHT))
+end
+function s.efop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	local e1=Effect.CreateEffect(rc)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.atkcon)
+	e1:SetValue(800)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+		if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		rc:RegisterEffect(e2,true)
+	end
+end
+function s.atkcon(e)
+	local ph=Duel.GetCurrentPhase()
+	local bc=e:GetHandler():GetBattleTarget()
+	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL) and bc
+end
+--
+function s.rpfilter(c)
+	return (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK)) and (c:IsLevelBelow(5) or c:IsRankBelow(5)) and not c:IsForbidden()
+end
+function s.tetg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return not e:GetHandler():IsForbidden() and Duel.IsExistingMatchingCard(s.rpfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+end
+function s.teop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoDeck(c,nil,1,REASON_EFFECT)
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+		local g=Duel.SelectMatchingCard(tp,s.rpfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+		local tc=g:GetFirst()
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
