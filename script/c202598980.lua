02edce5148056f65a62fc80db0e5a004155b4bc8
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-23 15:54:27 +0700

    Update c202598980.lua

--- script/c202598980.lua
+++ script/c202598980.lua
@@ -56,7 +56,8 @@ function s.tfilter(c,lv,e,tp)
 	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:GetLevel()==lv and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.tt(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.ttfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end	
+	if chk==0 then return Duel.IsExistingMatchingCard(s.ttfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) 
+	and Duel.IsExistingMatchingCard(s.tfilter,tp,LOCATION_DECK,0,1,e:GetHandler()) end	
 end
 function s.ot(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
