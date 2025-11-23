2f0906cbbcdca298002f2f1338f12a1869c77b2b
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-23 12:13:30 +0700

    Update c202598980.lua

--- script/c202598980.lua
+++ script/c202598980.lua
@@ -30,7 +30,7 @@ function s.rvfilter(c)
 	return c:IsSetCard(0x303)
 end
 function s.etfilter(c,e,tp)
-	return c:IsPreviousLocation(LOCATION_ONFIELD+LOCATION_GRAVE+LOCATION_REMOVED) and c:IsReason(REASON_EFFECT)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_EFFECT)
 end
 function s.con(e,tp,eg,ep,ev,re,r,rp)
 	return (eg:IsExists(s.etfilter,1,nil,e,tp) or eg:IsExists(s.etfilter,1,nil,e,1-tp)) and c:IsReason(REASON_EFFECT)
