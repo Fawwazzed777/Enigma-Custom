8925ab3f0bc776f4aae88552e40dc26702b7d002
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-23 14:12:52 +0700

    Update c202598980.lua

--- script/c202598980.lua
+++ script/c202598980.lua
@@ -48,15 +48,15 @@ end
 end
 --
 
-function s.rtfilter(c,ft)
+function s.rtfilter(c,lv,ft)
 	return c:IsFaceup() and c:IsMonster() and c:HasLevel() and c:IsSetCard(0x303) and c:IsAbleToDeck()
+	and Duel.IsExistingMatchingCard(s.tfilter,tp,LOCATION_DECK,0,1,nil,c:GetLevel())
 end
 function s.tfilter(c,sg,ft,e,lv,tp)
 	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_LIGHT) and not c:IsLevel(lv) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.ta(e,c,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tfilter,tp,LOCATION_DECK,0,1,nil,c:GetLevel())
-	and Duel.IsExistingMatchingCard(s.rtfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end	
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rtfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end	
 end
 function s.op(e,c,sg,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
