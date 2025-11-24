86cdc00dc1b4d3c82a17d9612fe6871b79e7ef3d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-24 18:27:18 +0700

    Update c202598980.lua

--- script/c202598980.lua
+++ script/c202598980.lua
@@ -58,7 +58,7 @@ function s.tt(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local c=e:GetHandler()
 	if chk==0 then return ft>-1 and Duel.IsExistingMatchingCard(s.rtfilter,tp,LOCATION_MZONE,0,1,c) and
-	Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e:GetHandler():GetCode()) end
+	Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e:GetHandler():GetCode(),e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_ONFIELD)
 end
 function s.ot(e,tp,eg,ep,ev,re,r,rp)
