7febd17ec57ac416d3a581a950780748fcc45980
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-24 20:25:49 +0700

    Update c202598980.lua

--- script/c202598980.lua
+++ script/c202598980.lua
@@ -75,7 +75,7 @@ function s.ot(e,tp,eg,ep,ev,re,r,rp)
 	local rc=e:GetLabelObject()
 	if not rc or Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,rc):GetFirst()
+	local sc=Duel.SelectMatchingCard(tp,s.rtfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,rc):GetFirst()
 	if sc then
 		Duel.ConfirmCards(1-tp,sc)
 		Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
