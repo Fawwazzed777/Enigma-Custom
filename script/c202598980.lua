a62bc0ce37e784646c3cb8c0cc8dbca526052844
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-24 20:24:03 +0700

    Update c202598980.lua

--- script/c202598980.lua
+++ script/c202598980.lua
@@ -48,46 +48,42 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 end
 --
-function s.spfilter(c,e,tp,code)
-	return c:IsSetCard(0x303) and not c:IsCode(code)
-	and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.spcfilter(c,e,tp,code)
+	return c:IsSetCard(0x303) and c:HasLevel() and Duel.GetMZoneCount(tp,c)>0
+	and Duel.IsExistingMatchingCard(s.rtfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c)
 end
 function s.rtfilter(c,ft,tp)
-	return c:IsFaceup() and c:IsSetCard(0x303) and c:IsAbleToDeck()
+	return c:IsFaceup() and c:IsSetCard(0x303) and c:IsAbleToDeck() and not c:IsLevel(tc:GetLevel())
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
---
-function s.deckspcostfilter(c,e,tp)
+{function s.deckspcostfilter(c,e,tp)
 	return c:IsAbleToDeckAsCost() and c:IsFaceup() and Duel.GetMZoneCount(tp,c)>0
 		and Duel.IsExistingMatchingCard(s.deckspfilter,tp,LOCATION_DECK,0,1,nil,e,tp,c:GetOriginalLevel())
 end
 function s.deckspfilter(c,e,tp,cod,lvl)
 	return not c:IsOriginalCode(cod) and c:IsLevel(lvl)
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.deckspcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.deckspcostfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local sc=Duel.SelectMatchingCard(tp,s.deckspcostfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp):GetFirst()
-	e:SetLabel(sc:GetOriginalCode(),sc:GetOriginalLevel())
-	Duel.SendtoDeck(sc,nil,2,REASON_COST)
-end
-
+end}
 function s.tt(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	local g=Duel.SelectMatchingCard(tp,s.spcfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	e:SetLabelObject(g:GetFirst())
+	Duel.SendtoDeck(g,nil,1,REASON_COST)
 end
 function s.ot(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local cod,lvl=e:GetLabel()
+	local rc=e:GetLabelObject()
+	if not rc or Duel.GetLocationCount(tp,LOCATION_MZONE)<1 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.deckspfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,cod,lvl)
-	if #g>0 then
-	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(500)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e:GetHandler():RegisterEffect(e1)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,rc):GetFirst()
+	if sc then
+		Duel.ConfirmCards(1-tp,sc)
+		Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		sc:RegisterEffect(e1)
 end
 end
