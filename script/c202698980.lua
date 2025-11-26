741ac227f004bcead65f3ba3fba9024052ab5b01
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-26 15:48:12 +0700

    Update c202698980.lua

--- script/c202698980.lua
+++ script/c202698980.lua
@@ -49,9 +49,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.IsExistingMatchingCard(s.amire,tp,LOCATION_MZONE,0,1,nil) 
 		and Duel.SelectYesNo(tp,aux.Stringid(id,1))then
 		local am=Duel.SelectMatchingCard(tp,s.rth,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-		if #am==0 then return end
+		if #am>0 then return end
 			Duel.HintSelection(am)
-			Duel.BreakEffect()
 			Duel.SendtoDeck(am,nil,1,REASON_EFFECT)
 		
 
