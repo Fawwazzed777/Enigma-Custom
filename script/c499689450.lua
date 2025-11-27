ae36093461ec930b1913c6c742c90cef30e92aae
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-27 11:09:44 +0700

    Update c499689450.lua

--- script/c499689450.lua
+++ script/c499689450.lua
@@ -58,7 +58,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,tc,1,0,0)
 	if tc then
 	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 end
