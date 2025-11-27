ea9acae582d3ec9f082195c5f5d1becfe86a0ba7
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-27 10:24:21 +0700

    Update

---
+++ script/c499689450.lua
@@ -0,0 +1,37 @@
+--Noble Tempest Breaker
+local s,id=GetID()
+function s.initial_effect(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_INACTIVATE+EFFECT_FLAG_CANNOT_NEGATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+	e1:SetCountLimit(1,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={49968945}
+function s.atkfilter(c)
+	return c:IsCode(49968945) or (c:IsType(TYPE_SYNCHRO) and c:ListsCode(49968945))
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(s.atkfilter),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
+	if #g>0 then
+	Duel.HintSelection(g)
+	if Duel.SendtoDeck(g,nil,1,REASON_EFFECT)~=0 then
+	Duel.BreakEffect()
+	Duel.Recover(tp,1000,REASON_EFFECT)
+end
+end
+end
