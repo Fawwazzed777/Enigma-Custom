bdf3ff458ec21d4c2ef3594a28c2abc07da7a5bb
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-11-27 11:09:03 +0700

    Update c499689450.lua

--- script/c499689450.lua
+++ script/c499689450.lua
@@ -54,13 +54,12 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local tc=Duel.GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 	if tc then
-	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 
+	Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 end
 end
