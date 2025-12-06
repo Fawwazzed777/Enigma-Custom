10fedc316145220ab78e94f91632d8d90860c68e
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-06 13:40:40 +0700

    Update c202998980.lua

--- script/c202998980.lua
+++ script/c202998980.lua
@@ -4,8 +4,40 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Xyz summon
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.ffilter),4,2,nil,nil,Xyz.InfiniteMats)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(Cost.DetachFromSelf(1,1,nil))
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
 end
 s.listed_series={0x303,0x344}
 function s.ffilter(c,fc,sumtype,tp)
 	return c:IsSetCard(0x303) or c:IsSetCard(0x344)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,1,nil) end
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_MZONE,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_MZONE,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.HintSelection(g)
+		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 and tc:IsSetCard(0x303) then
+		local sg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
+		if sg then
+		if Duel.SendtoDeck(sg,1,nil,REASON_EFFECT)>0 then
+		Duel.Draw(1,tp,REASON_EFFECT)
+end
+end
+end
+end
 end
