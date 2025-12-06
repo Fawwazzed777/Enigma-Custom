bd5ec0560b40152e6631ad4b2dddb9e4a2d83d5f
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-06 14:51:49 +0700

    Update c202998980.lua

--- script/c202998980.lua
+++ script/c202998980.lua
@@ -44,11 +44,10 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		if Duel.SendtoDeck(sg,nil,1,REASON_EFFECT)>0 then
 		Duel.BreakEffect()
 		Duel.Draw(tp,1,REASON_EFFECT)
-	else 
 	--Enigmation
-	if tc:IsSetCard(0x344) then
+	if tc then
 		Duel.HintSelection(g)
-		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
+		if Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 and tc:IsSetCard(0x344) then
 		local rg=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,nil)
 		if rg then
 		Duel.Remove(rg,nil,1,REASON_EFFECT)
