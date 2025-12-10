dce4e0fab016a43c6b821dc40a0b6c53e1e3ab62
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 01:25:38 +0700

    Update c999110001.lua

--- script/c999110001.lua
+++ script/c999110001.lua
@@ -1,9 +1,8 @@
 if not GenerateEffect then
 	GenerateEffect={}
-	
-	end
---Additional ATTRIBUTE
-ATTRIBUTE_RADIANT= 0x94
+	--Additional ATTRIBUTE
+	ATTRIBUTE_RADIANT= 0x94
 
---Additional Types
-RACE_VIRTUOUS    = 0x1c0101
+	--Additional Types
+	RACE_VIRTUOUS    = 0x1c0101
+	end
