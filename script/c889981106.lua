f06a6e1d20ac428cf4855b49602f97e31345384d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 14:17:31 +0700

    fIX

---
+++ script/c889981106.lua
@@ -0,0 +1,86 @@
+--Enigmatic Lord - Alpha
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--special summon
+	local es=Effect.CreateEffect(c)
+	es:SetDescription(aux.Stringid(id,0))
+	es:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	es:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_F)
+	es:SetCode(EVENT_PHASE|PHASE_BATTLE_START)
+	es:SetRange(LOCATION_HAND|LOCATION_REMOVED)
+	es:SetCountLimit(1,{id,0})
+	es:SetTarget(s.sptg)
+	es:SetOperation(s.spop)
+	c:RegisterEffect(es)
+	local er=es:Clone()
+	er:SetCode(EVENT_PHASE|PHASE_STANDBY)
+	c:RegisterEffect(er)
+	local et=es:Clone()
+	et:SetCode(EVENT_PHASE|PHASE_END)
+	c:RegisterEffect(et)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetTarget(s.dtg)
+	e1:SetOperation(s.dop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e3=e1:Clone()
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_REMOVE+CATEGORY_DAMAGE)
+	e4:SetType(EFFECT_TYPE_SINGLE|EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCondition(function(e,tp) return Duel.GetAttacker():IsControler(1-tp) end)
+	e4:SetTarget(s.tr)
+	e4:SetOperation(s.op)
+	c:RegisterEffect(e4)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+	end
+end
+function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+function s.dop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+function s.tr(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local bc=Duel.GetAttacker()
+	e:SetLabelObject(bc)
+	bc:CreateEffectRelation(e)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,1000)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetLabelObject()
+	if bc:IsRelateToEffect(e) and bc:IsMonster() and Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)>0 then
+		Duel.BreakEffect()
+		Duel.Damage(1-tp,1000,REASON_EFFECT)
+	end
+end
