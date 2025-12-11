534b999c3d97d140ed77cb58d13cee13f095d2e7
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 13:42:34 +0700

    Fix

--- script/c889981105.lua
+++ script/c889981105.lua
@@ -78,16 +78,6 @@ end
 function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if re:GetHandler():IsRelateToEffect(re) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
-		e1:SetValue(1000)
-		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		c:RegisterEffect(e2)
+		Duel.Destroy(re,REASON_EFFECT)
 end
 end
