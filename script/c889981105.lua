e97d71c5978b8b3dc9751d860e8f5cb20074df9a
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 10:53:12 +0700

    Update c889981105.lua

--- script/c889981105.lua
+++ script/c889981105.lua
@@ -22,7 +22,5 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		c:CompleteProcedure()
-	end
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
