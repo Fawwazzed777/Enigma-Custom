fd09d76fec1871ff23791c0f9a61977fa498bd1e
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 12:13:59 +0700

    Fix

--- script/c889981105.lua
+++ script/c889981105.lua
@@ -3,26 +3,39 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
 	--special summon
+	local es=Effect.CreateEffect(c)
+	es:SetDescription(aux.Stringid(id,0))
+	es:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	es:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_F)
+	es:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	es:SetRange(LOCATION_HAND+LOCATION_REMOVED)
+	es:SetCountLimit(1,id)
+	es:SetTarget(s.sptg)
+	es:SetOperation(s.spop)
+	c:RegisterEffect(es)
+	local er=es:Clone()
+	er:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	c:RegisterEffect(er)
+	local et=es:Clone()
+	et:SetCode(EVENT_PHASE+PHASE_END)
+	c:RegisterEffect(et)
+	--
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
-	e1:SetRange(LOCATION_HAND+LOCATION_REMOVED)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetTarget(s.dtg)
+	e1:SetOperation(s.dop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 	local e3=e1:Clone()
-	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e3)
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -32,6 +45,21 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+	end
+end
+function s.filter(c,e,tp)
+	return c:IsDestructable() and not c:IsImmuneToEffect(e)
+end
+function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.dop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.HintSelection(g)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
