d48cb3a790639973ba487830d9b61a1b5ee890ed
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 10:25:03 +0700

    Update c889981105.lua

--- script/c889981105.lua
+++ script/c889981105.lua
@@ -7,14 +7,28 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
-	e1:SetRange(LOCATION_HAND+LOCATION_REMOVED)
-	e1:SetTargetRange(POS_FACEUP_ATTACK,1)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_HAND|LOCATION_REMOVED)
 	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
 function s.spcon(e,c)
 	if c==nil then return Duel.IsPhase(PHASE_STANDBY+PHASE_BATTLE_START+PHASE_END) end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,true) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,true,true,POS_FACEUP)~=0 then
+		c:CompleteProcedure()
+	end
 end
