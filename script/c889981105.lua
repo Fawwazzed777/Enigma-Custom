80ab948e080b038e100469c65bedae366239eb49
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-11 11:16:36 +0700

    Update c889981105.lua

--- script/c889981105.lua
+++ script/c889981105.lua
@@ -6,7 +6,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e1:SetRange(LOCATION_HAND|LOCATION_REMOVED)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
@@ -25,7 +25,5 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP) 
-end
 end
