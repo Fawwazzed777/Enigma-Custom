204cd0ebd701cb517173ffa02d025814c0685c99
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-12 19:08:12 +0700

    Updates

--- script/c889981111.lua
+++ script/c889981111.lua
@@ -7,11 +7,16 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={889981112}
+s.listed_names={889981110,889981112}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return (Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,889981110),tp,LOCATION_ONFIELD,0,1,nil)
+	or Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0)
+end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -33,10 +38,16 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 			e0:SetValue(1)
 			e0:SetReset(RESET_EVENT|RESETS_STANDARD)
-			token:RegisterEffect(e0)
-			Duel.BreakEffect()
-			Duel.Draw(tp,1,REASON_EFFECT)
+			if token:RegisterEffect(e0)~=0 then
+			local tc=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD):RandomSelect(tp,1)
+			if tc then
+			Duel.HintSelection(tc)
+			Duel.Destroy(tc,REASON_EFFECT)
+				Duel.BreakEffect()
+				Duel.Draw(tp,1,REASON_EFFECT)
 		end
 		Duel.SpecialSummonComplete()
+end
+end
 end
 end
