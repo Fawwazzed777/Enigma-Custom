682173b94af690f94073aad0521fede22e8322ad
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-12 17:50:21 +0700

    Update

---
+++ script/c889981111.lua
@@ -0,0 +1,42 @@
+--Phantom Pressure
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+end
+s.listed_names={889981112}
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,TYPES_TOKEN,3000,3000,10,RACE_ROCK,ATTRIBUTE_DARK) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,TYPES_TOKEN,3000,3000,10,RACE_ROCK,ATTRIBUTE_DARK) then
+		for i=1,2 do
+			local token=Duel.CreateToken(tp,id+1)
+			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+			Duel.BreakEffect()
+			Duel.Draw(tp,1,REASON_EFFECT)
+			--cannot be summon material
+			local e0=Effect.CreateEffect(c)
+			e0:SetType(EFFECT_TYPE_SINGLE)
+			e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+			e0:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+			e0:SetValue(1)
+			c:RegisterEffect(e0)
+			Duel.Remove(e:GetHandler(),REASON_EFFECT)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
