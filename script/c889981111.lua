bbbf9e9888271dd0d64a709693e059eefa829164
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-12 18:06:20 +0700

    Update c889981111.lua

--- script/c889981111.lua
+++ script/c889981111.lua
@@ -10,20 +10,19 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-
 end
 s.listed_names={889981112}
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,TYPES_TOKEN,3000,3000,10,RACE_ROCK,ATTRIBUTE_DARK) end
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,889981112,TYPES_TOKEN,3000,3000,10,RACE_ROCK,ATTRIBUTE_DARK) end
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,TYPES_TOKEN,3000,3000,10,RACE_ROCK,ATTRIBUTE_DARK) then
-			local token=Duel.CreateToken(tp,id+1)
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,889981112,TYPES_TOKEN,3000,3000,10,RACE_ROCK,ATTRIBUTE_DARK) then
+			local token=Duel.CreateToken(tp,889981112)
 			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP_ATTACK)
 			Duel.BreakEffect()
 			Duel.Draw(tp,1,REASON_EFFECT)
