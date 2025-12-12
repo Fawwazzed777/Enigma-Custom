656a79550beb852757a7bd870bdfc6472c20081a
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-12 16:58:22 +0700

    Updates

--- script/c889981110.lua
+++ script/c889981110.lua
@@ -1,4 +1,4 @@
---Enigmatic Lord - Alpha
+--Enigmatic Lord – Omega
 local s,id=GetID()
 function s.initial_effect(c)
 	c:SetUniqueOnField(1,0,id)
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	es:SetDescription(aux.Stringid(id,0))
 	es:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	es:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_F)
-	es:SetCode(EVENT_PHASE|PHASE_STANDBY)
+	es:SetCode(EVENT_PHASE|PHASE_BATTLE)
 	es:SetRange(LOCATION_HAND|LOCATION_REMOVED)
 	es:SetCountLimit(1,{id,0})
 	es:SetTarget(s.sptg)
@@ -30,20 +30,19 @@ function s.initial_effect(c)
 	local e3=e1:Clone()
 	e3:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e3)
-	--
+	--TOKEN
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetCategory(CATEGORY_DESTROY)
-	e4:SetType(EFFECT_TYPE_FIELD|EFFECT_TYPE_TRIGGER_F)
-	e4:SetCode(EVENT_CHAINING)
-	e4:SetCountLimit(1,{id,2})
-	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP|EFFECT_FLAG_DAMAGE_CAL)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_DESTROYED+EVENT_REMOVE)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetCondition(s.dtcon)
-	e4:SetTarget(s.destg)
-	e4:SetOperation(s.drop)
+	e4:SetCondition(s.con)
+	e4:SetTarget(s.tg)
+	e4:SetOperation(s.op)
 	c:RegisterEffect(e4)
 end
+s.listed_names={889981111}
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
@@ -56,30 +55,32 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,0,LOCATION_ONFIELD,nil)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,nil)
 	if chk==0 then return #g>0 end
 end
 function s.dop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,0,LOCATION_ONFIELD,1,1,nil)
-	if #g>0 then
-		Duel.HintSelection(g)
-		Duel.Destroy(g,REASON_EFFECT)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local tc=Duel.GetFieldGroup(tp,0,LOCATION_ONFIELD):RandomSelect(tp,1)
+	if tc then
+		Duel.HintSelection(tc)
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
 	end
 end
-function s.dtcon(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and re:GetHandler():IsLocation(LOCATION_ONFIELD)
+function s.filter(c)
+	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsReason(REASON_BATTLE|REASON_EFFECT)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and not chkc:IsControler(tp) end
-	local rc=re:GetHandler()
-	if chk==0 then return rc:IsOnField() and rc:IsControler(1-tp) end
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,rc,1,0,LOCATION_ONFIELD)
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter,1,nil)
 end
-function s.drop(e,tp,eg,ep,ev,re,r,rp)
-	local rc=re:GetHandler()
-	if Duel.Destroy(rc,REASON_EFFECT)>0 then
-	Duel.BreakEffect()
-	Duel.Recover(tp,1000,REASON_EFFECT)
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local token1={889981111}
+	local tc=Duel.CreateToken(tp,token1)
+	if tc:IsRelateToEffect(e) and tc:IsAbleToHand() then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 end
 end
