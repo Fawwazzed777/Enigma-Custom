fd6e63cd4244ddf5c5b77884602af74c162bcd85
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-17 13:55:37 +0700

    Fix

---
+++ script/custom test/c214399999.lua
@@ -0,0 +1,60 @@
+--Eternity Ace  - Chrono Zereya 
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Must be special summoned by its own method
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_EXTRA)
+	e2:SetCondition(s.sprcon)
+	e2:SetTarget(s.sprtg)
+	e2:SetOperation(s.sprop)
+	c:RegisterEffect(e2)
+end
+function s.sprfilter(c)
+	return c:IsFaceup() and c:HasRank() and c:IsAbleToGraveAsCost()
+end
+function s.sprfilter1(c,tp,g,sc)
+	local rk=c:GetRank()
+	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE,0,nil)
+	return c:IsType(TYPE_XYZ) and g:IsExists(s.sprfilter2,1,c,tp,c,sc,rk)
+end
+function s.sprfilter2(c,tp,mc,sc,rk)
+	local sg=Group.FromCards(c,mc)
+	return (math.abs((c:GetRank()-mc:GetLevel()))==4) and not c:IsType(TYPE_XYZ) and Duel.GetLocationCountFromEx(tp,tp,sg,sc)>0
+end
+function s.sprcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE,0,nil)
+	return g:IsExists(s.sprfilter1,1,nil,tp,g,c)
+end
+function s.sprtg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetMatchingGroup(s.sprfilter,tp,LOCATION_MZONE,0,nil)
+	local g1=g:Filter(s.sprfilter1,nil,tp,g,c)
+	local mg1=aux.SelectUnselectGroup(g1,e,tp,1,1,nil,1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	if #mg1>0 then
+		local mc=mg1:GetFirst()
+		local g2=g:Filter(s.sprfilter2,mc,tp,mc,c,mc:GetLevel())
+		local mg2=aux.SelectUnselectGroup(g2,e,tp,1,1,nil,1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+		mg1:Merge(mg2)
+	end
+	if #mg1==2 then
+		mg1:KeepAlive()
+		e:SetLabelObject(mg1)
+		return true
+	end
+	return false
+end
+function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_COST)
+end
