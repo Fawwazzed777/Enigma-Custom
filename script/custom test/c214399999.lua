b738d7fb7650f786f9262a93a572768cdb85d52d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-17 21:20:26 +0700

    Update c214399999.lua

--- script/custom test/c214399999.lua
+++ script/custom test/c214399999.lua
@@ -27,7 +27,7 @@ function s.xyzop(e,tp,chk,mc)
 	if chk==0 then return not Duel.HasFlagEffect(tp,id) and Duel.IsExistingMatchingCard(s.eafilter,tp,LOCATION_SZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
 	local sc=Duel.GetMatchingGroup(s.eafilter,tp,LOCATION_SZONE,0,nil):SelectUnselect(Group.CreateGroup(),tp,false,Xyz.ProcCancellable)
-	if sc and c:SetMaterial(sc)>0 then	
+	if sc and Duel.Remove(sc,POS_FACEUP,REASON_REMOVE|REASON_COST)>0 then	
 		return Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,EFFECT_FLAG_OATH,1)
 	else
 		return false
