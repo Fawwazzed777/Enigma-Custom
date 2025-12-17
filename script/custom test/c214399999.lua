ca880cd5dc11dc1d4b4678d3b7961eb677a72355
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-17 15:31:17 +0700

    Update c214399999.lua

--- script/custom test/c214399999.lua
+++ script/custom test/c214399999.lua
@@ -26,7 +26,7 @@ function s.sprfilter1(c,tp,g,sc)
 end
 function s.sprfilter2(c,tp,mc,sc,lv)
 	local sg=Group.FromCards(c,mc)
-	return (math.abs((c:GetLevel()+mc:GetRank()))==3) and Duel.GetLocationCountFromEx(tp,tp,sg,sc)>0
+	return (c:GetLevel()+mc:GetRank())==3 and Duel.GetLocationCountFromEx(tp,tp,sg,sc)>0
 end
 function s.sprcon(e,c)
 	if c==nil then return true end
