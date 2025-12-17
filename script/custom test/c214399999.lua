551628aedcd8d888c5abf20124d676c8ff228b2a
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-18 00:21:16 +0700

    Updates

--- script/custom test/c214399999.lua
+++ script/custom test/c214399999.lua
@@ -3,17 +3,41 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x994),7,3,s.ovfilter,aux.Stringid(id,0),3,s.xyzop,Xyz.InfiniteMats)
-	--Special summoned by its own method
+	--Special summon by its own method
 	local e0=Effect.CreateEffect(c)
 	e0:SetDescription(aux.Stringid(id,0))
 	e0:SetType(EFFECT_TYPE_FIELD)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e0:SetCode(EFFECT_SPSUMMON_PROC)
-	e0:SetRange(LOCATION_GRAVE)
+	e0:SetRange(LOCATION_PZONE)
+	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e0:SetCondition(s.sprcon)
 	e0:SetTarget(s.sprtg)
 	e0:SetOperation(s.sprop)
 	c:RegisterEffect(e0)
+	--Place
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_SEARCH)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.drcon)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+	--pendulum zone
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(s.pencon)
+	e2:SetTarget(s.pentg)
+	e2:SetOperation(s.penop)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x993,0x994,15799999}
 function s.ovfilter(c,tp,xyzc)
@@ -33,7 +57,7 @@ function s.xyzop(e,tp,chk,mc)
 	end
 end
 function s.sprfilter(c,e)
-	return c:IsFaceup() and c:IsSetCard(0x993) and c:IsCanBeXyzMaterial()
+	return c:IsFaceup() and c:IsMonster() and c:IsCanBeXyzMaterial()
 end
 function s.sprfilter1(c,tp,g,sc)
 	local lv=c:GetLevel()
@@ -42,7 +66,7 @@ function s.sprfilter1(c,tp,g,sc)
 end
 function s.sprfilter2(c,tp,mc,sc,lv)
 	local sg=Group.FromCards(c,mc)
-	return (c:GetLevel()+mc:GetRank())==7 and Duel.GetLocationCountFromEx(tp,tp,sg,sc)>0
+	return (c:GetLevel()+mc:GetLevel())==7 
 end
 function s.sprcon(e,c)
 	if c==nil then return true end
@@ -82,4 +106,49 @@ function s.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	end
 	Duel.Overlay(c,g)
 	c:CompleteProcedure()
+end
+--
+function s.pfilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsType(TYPE_CONTINUOUS) and c:IsSetCard(0x993) and not c:IsForbidden() and c:CheckUniqueOnField(tp)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tsfilter(chkc,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(s.pfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,e:GetHandler(),tp) end
+	
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local tc=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.pfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,e:GetHandler(),tp):GetFirst()
+	if tc then
+		if Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)>0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetValue(s.atkup)
+		Duel.RegisterEffect(e1,tp)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		Duel.RegisterEffect(e2,tp)
+end
+end
+end
+function s.atkup(e,c)
+	return Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsSetCard,0x993),c:GetControler(),LOCATION_REMOVED,0,nil)*300
+end
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0 and c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLocation(tp,LOCATION_PZONE,0) or Duel.CheckLocation(tp,LOCATION_PZONE,1) end
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckLocation(tp,LOCATION_PZONE,0) and not Duel.CheckLocation(tp,LOCATION_PZONE,1) then return false end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
 end
