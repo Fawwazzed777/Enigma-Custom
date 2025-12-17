8a01507340b38b0eb2a2d95a588b260009a295b0
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-17 21:34:51 +0700

    Update c214399999.lua

--- script/custom test/c214399999.lua
+++ script/custom test/c214399999.lua
@@ -20,14 +20,13 @@ function s.ovfilter(c,tp,xyzc)
 	return c:IsSummonCode(xyzc,SUMMON_TYPE_XYZ,tp,15799999) and c:IsFaceup()
 end
 function s.eafilter(c,tp)
-	return c:IsSetCard(0x993) and c:IsFaceup() and c:IsCanBeXyzMaterial()
+	return c:IsSetCard(0x993) and c:IsFaceup()
 end
 function s.xyzop(e,tp,chk,mc)
-	local c=e:GetHandler()
-	if chk==0 then return not Duel.HasFlagEffect(tp,id) and Duel.IsExistingMatchingCard(s.eafilter,tp,LOCATION_SZONE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local sc=Duel.GetMatchingGroup(s.eafilter,tp,LOCATION_SZONE,0,nil):SelectUnselect(Group.CreateGroup(),tp,false,Xyz.ProcCancellable)
-	if sc and Duel.Remove(sc,POS_FACEUP,REASON_COST)>0 then	
+	if chk==0 then return not Duel.HasFlagEffect(tp,id) and Duel.IsExistingMatchingCard(s.eafilter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISCARD)
+	local sc=Duel.GetMatchingGroup(s.eafilter,tp,LOCATION_ONFIELD,0,nil):SelectUnselect(Group.CreateGroup(),tp,false,Xyz.ProcCancellable)
+	if sc and Duel.SendtoGrave(sc,POS_FACEUP,REASON_COST)>0 then
 		return Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,EFFECT_FLAG_OATH,1)
 	else
 		return false
