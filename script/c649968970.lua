7e837b2adb1e5803efce7a897415a30c99abbde6
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-19 19:34:17 +0700

    Fix

--- script/c649968970.lua
+++ script/c649968970.lua
@@ -4,6 +4,15 @@ function s.initial_effect(c)
 	--synchro summon
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_WATER),1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(function(e) return e:GetHandler():IsSynchroSummoned() end)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
 
 end
 s.listed_series={0xfc13}
