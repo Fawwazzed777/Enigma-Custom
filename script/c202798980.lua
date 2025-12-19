27a214d7ad2dfe66b9ad4cb79c0b148bd415ad7b
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-19 18:42:17 +0700

    Update c202798980.lua

--- script/c202798980.lua
+++ script/c202798980.lua
@@ -63,7 +63,8 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirst()
 	local ct=Duel.GetMatchingGroupCount(aux.TRUE,tp,LOCATION_REMOVED,LOCATION_REMOVED,nil)
-	if tc=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c) then
+	local tc=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c) 
+	if tc then
 	Duel.HintSelection(tc)
 		--Decrease ATK
 		local e1=Effect.CreateEffect(c)
