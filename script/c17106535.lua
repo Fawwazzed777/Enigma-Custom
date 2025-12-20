d7630463bc5c87088a14ffc38d96731da86734f9
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-20 18:47:40 +0700

    Fix

--- script/c17106535.lua
+++ script/c17106535.lua
@@ -6,7 +6,11 @@ function s.initial_effect(c)
 	c:SetSPSummonOnce(id)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixRep(c,true,true,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),2,99,s.mfilter)
+	local f0=Fusion.AddProcMixRep(c,true,true,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),2,99,s.mfilter)[1]
+	f0:SetDescription(aux.Stringid(id,2))
+	--Fusion Summon procedure (3 "Blue-Eyes" monsters)
+	local f1=Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),2,99,s.xfilter,1)[1]
+	f1:SetDescription(aux.Stringid(id,3))
 	--cannot be fusion material
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
@@ -34,9 +38,12 @@ function s.initial_effect(c)
 	e2:SetValue(aux.DarkLightFLimit)
 	c:RegisterEffect(e2)
 end
-s.listed_names={17106529}
+s.listed_names={17106529,96488201}
 function s.mfilter(c,sc,st,tp)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_FUSION) and c:IsLevelAbove(8)
+end
+function s.xfilter(c,fc,sumtype,tp)
+	return c:IsCode(96488201)
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
