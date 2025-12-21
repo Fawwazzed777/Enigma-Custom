218327b17802e8fa84d09b8454b1a17d8dc755ee
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-21 21:03:03 +0700

    Update c96488223.lua

--- script/c96488223.lua
+++ script/c96488223.lua
@@ -3,7 +3,7 @@ Duel.LoadScript("proc_xyz_phantasm.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
-	aux.XyzPhantasm.AddProcedure(c,function(c) return c:IsFaceup() and c:IsSetCard(0x145) and c:IsType(TYPE_XYZ) end,5,aux.Stringid(id,2))
+	aux.XyzPhantasm.AddProcedure(c,function(c) return c:IsFaceup() and c:IsSetCard(0x145) and c:IsType(TYPE_XYZ) end,2,aux.Stringid(id,2))
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.xp),12,3,nil,nil,3,Xyz.InfiniteMats)
 	c:EnableReviveLimit()
 	--
