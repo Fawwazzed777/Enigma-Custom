6597b8d550872ae88aa706ff1a0632e8b6fdc550
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-21 23:21:21 +0700

    Update proc_xyz_phantasm.lua

--- script/proc_xyz_phantasm.lua
+++ script/proc_xyz_phantasm.lua
@@ -1,47 +1,37 @@
-aux.XyzPhantasm = aux.XyzPhantasm or {}
-local XyzPhantasm = aux.XyzPhantasm
+-- proc_xyz_alt.lua
+aux.XyzAlt = aux.XyzAlt or {}
+local XyzAlt = aux.XyzAlt
+-- filter: monster yang boleh dipakai sebagai ALT material
+-- banishcount: minimal kartu banish
+-- desc: stringid
+function XyzAlt.AddProcedure(c,filter,banishcount,desc)
+	Xyz.AddProcedure(
+		c,
+		nil,
+		c:GetRank(),0,
+		function(tc,tp,lc)
+			return filter(tc,tp,lc)
+				and Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)>=banishcount
+		end,
+		function(e,tp,chk)
+			if chk==0 then return true end
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+			local tc=Duel.SelectMatchingCard(
+				tp,
+				function(c) return filter(c,tp,e:GetHandler()) end,
+				tp,LOCATION_MZONE,0,1,1,nil
+			):GetFirst()
+			if not tc then return false end
 
-function XyzPhantasm.AddProcedure(c,filter,banishcount,desc)
-	local e=Effect.CreateEffect(c)
-	e:SetType(EFFECT_TYPE_FIELD)
-	e:SetCode(EFFECT_SPSUMMON_PROC)
-	e:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
-	e:SetRange(LOCATION_EXTRA)
-	e:SetValue(SUMMON_TYPE_XYZ)
-	e:SetCondition(XyzPhantasm.con(filter,banishcount))
-	e:SetOperation(XyzPhantasm.op(filter))
-	if desc then
-		e:SetDescription(desc)
-	end
-	c:RegisterEffect(e)
-end
-
-function XyzPhantasm.con(filter,banishcount)
-	return function(e,c)
-		if c==nil then return true end
-		local tp=c:GetControler()
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(filter,tp,LOCATION_MZONE,0,1,nil)
-			and Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)>=banishcount
-	end
-end
-
-function XyzPhantasm.op(filter)
-	return function(e,tp,eg,ep,ev,re,r,rp,c)
-		if not Duel.SelectYesNo(tp,aux.Stringid(c:GetCode(),0)) then
-			return false
-		end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-		local g=Duel.SelectMatchingCard(tp,filter,tp,LOCATION_MZONE,0,1,1,nil,true)
-		if #g==0 then return false end
-		local tc=g:GetFirst()
-		local mg=tc:GetOverlayGroup()
-		if #mg>0 then
-			Duel.Overlay(c,mg)
-		end
-		c:SetMaterial(Group.FromCards(tc))
-		Duel.Overlay(c,Group.FromCards(tc))
-		c:CompleteProcedure()
-		--return true--
-	end
+			local c=e:GetHandler()
+			local mg=tc:GetOverlayGroup()
+			if #mg>0 then
+				Duel.Overlay(c,mg)
+			end
+			c:SetMaterial(Group.FromCards(tc))
+			Duel.Overlay(c,Group.FromCards(tc))
+			return true
+		end,
+		desc
+	)
 end
