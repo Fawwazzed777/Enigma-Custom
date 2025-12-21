0f1ce58f5226233d68f383303b9b183f3642fb3a
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-22 01:24:54 +0700

    Update proc_xyz_alt.lua

--- script/proc_xyz_alt.lua
+++ script/proc_xyz_alt.lua
@@ -1,31 +1,48 @@
 aux.XyzAlt=aux.XyzAlt or {}
 local XyzAlt=aux.XyzAlt
+-- filter: monster Xyz yang boleh dipakai
+-- banishcount: minimal kartu face-up banished
+-- desc: StringID
 function XyzAlt.AddProcedure(c,filter,banishcount,desc)
-	Xyz.AddProcedure(
-		c,
-		nil,
-		c:GetRank(),0,
-		function(tc,tp,lc)
-			return filter(tc,tp,lc)
-				and Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)>=banishcount
-		end,
-		function(e,tp,chk)
-			if chk==0 then return true end
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-			local tc=Duel.SelectMatchingCard(
-				tp,
-				function(c) return filter(c,tp,e:GetHandler()) end,
-				tp,LOCATION_MZONE,0,1,1,nil
-			):GetFirst()
-			if not tc then return false end
+	local e=Effect.CreateEffect(c)
+	e:SetType(EFFECT_TYPE_FIELD)
+	e:SetCode(EFFECT_SPSUMMON_PROC)
+	e:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_SPSUM_PARAM)
+	e:SetRange(LOCATION_EXTRA)
+	e:SetValue(SUMMON_TYPE_XYZ)
+	e:SetCondition(XyzAlt.con(filter,banishcount))
+	e:SetOperation(XyzAlt.op(filter))
+	if desc then
+		e:SetDescription(desc)
+	end
+	c:RegisterEffect(e)
+end
 
-			local c=e:GetHandler()
-			local mg=tc:GetOverlayGroup()
-			if #mg>0 then
-				Duel.Overlay(c,mg)
-			end
-			c:SetMaterial(Group.FromCards(tc))
-			Duel.Overlay(c,Group.FromCards(tc))
-			return true
-		end)
+function XyzAlt.con(filter,banishcount)
+	return function(e,c)
+		if c==nil then return true end
+		local tp=c:GetControler()
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(filter,tp,LOCATION_MZONE,0,1,nil,tp)
+			and Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)>=banishcount
+	end
+end
+
+function XyzAlt.op(filter)
+	return function(e,tp,eg,ep,ev,re,r,rp,c)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local tc=Duel.SelectMatchingCard(
+			tp,
+			function(tc) return filter(tc,tp) end,
+			tp,LOCATION_MZONE,0,1,1,nil
+		):GetFirst()
+		if not tc then return end
+		local mg=tc:GetOverlayGroup()
+		if #mg>0 then
+			Duel.Overlay(c,mg)
+		end
+		c:SetMaterial(tc)
+		Duel.Overlay(c,tc)
+		c:CompleteProcedure()
+	end
 end
