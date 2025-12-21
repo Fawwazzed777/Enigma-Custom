9ff8e3c0e3c8ea5b3180be447193137307ab7c64
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-22 04:53:54 +0700

    Update proc_xyz_unified.lua

--- script/proc_xyz_unified.lua
+++ script/proc_xyz_unified.lua
@@ -1,39 +1,32 @@
+--proc_xyz_unified.lua
 aux.XyzUnified = aux.XyzUnified or {}
-local XU = aux.XyzUnified
+local XyzUnified = aux.XyzUnified
 
-function XU.AddProcedure(c,lvfilter,rank,minct,alt)
-	Xyz.AddProcedure(
-		c,
-		lvfilter,
-		rank,
-		minct,
-		function(tc,tp,lc)
-			if not alt then return false end
-			return tc:IsFaceup()
-				and alt.filter(tc,tp,lc)
-				and Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)>=alt.banish
-		end,
-		function(e,tp,chk,lc)
-			if chk==0 then return true end
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-			local tc=Duel.SelectMatchingCard(
-				tp,
-				function(tc) return alt.filter(tc,tp,e:GetHandler()) end,
-				tp,LOCATION_MZONE,0,1,1,nil
-			):GetFirst()
-			if not tc then return false end
+function XyzUnified.AltXyzSummon(c,filter,banishcount,desc)
+	local e=Effect.CreateEffect(c)
+	e:SetDescription(desc)
+	e:SetType(EFFECT_TYPE_IGNITION)
+	e:SetRange(LOCATION_EXTRA)
+	e:SetCondition(function(e,tp)
+		return Duel.IsExistingMatchingCard(filter,tp,LOCATION_MZONE,0,1,nil,tp)
+			and Duel.GetMatchingGroupCount(Card.IsFaceup,tp,LOCATION_REMOVED,0,nil)>=banishcount
+	end)
+	e:SetOperation(function(e,tp)
+		local c=e:GetHandler()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local tc=Duel.SelectMatchingCard(tp,filter,tp,LOCATION_MZONE,0,1,1,nil,tp):GetFirst()
+		if not tc then return end
 
-			local c=e:GetHandler()
-			local mg=tc:GetOverlayGroup()
-			if #mg>0 then
-				Duel.Overlay(c,mg)
-			end
-			c:SetMaterial(Group.FromCards(tc))
-			Duel.Overlay(c,Group.FromCards(tc))
-			Duel.XyzSummonComplete(c)
-			return true
-		end,
-		alt.desc,
-		Xyz.InfiniteMats
-	)
+		local mg=tc:GetOverlayGroup()
+		if #mg>0 then
+			Duel.Overlay(c,mg)
+		end
+		c:SetMaterial(Group.FromCards(tc))
+		Duel.Overlay(c,Group.FromCards(tc))
+
+		if Duel.SpecialSummon(c,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
+			c:CompleteProcedure()
+		end
+	end)
+	c:RegisterEffect(e)
 end
