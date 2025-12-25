e811fc5625444ffe76e17d2cd833493915f6c6ef
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-25 14:33:16 +0700

    Update c649968970.lua

--- script/Unfold/c649968970.lua
+++ script/Unfold/c649968970.lua
@@ -70,7 +70,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 	if Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
 	-- cannot summon extra deck monsters except WATER synchro
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
