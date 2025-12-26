8219cc60d34d436fdeb7ef11778c60103b113739
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-26 08:14:25 +0700

    Update c499689723.lua

--- script/Unfold/c499689723.lua
+++ script/Unfold/c499689723.lua
@@ -24,36 +24,34 @@ function s.spfilter(c,e,tp)
 	and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
-	local codes={}
-	local uniq=0
-	for tc in g:Iter() do
-		local code=tc:GetCode()
-		if not codes[code] then
-			codes[code]=true
-			uniq=uniq+1
-		end
+	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,nil,e,tp)
+	local ct=math.min(#sg,Duel.GetLocationCount(tp,LOCATION_MZONE))
+	if chk==0 then return ct>0 and Duel.CheckLPCost(tp,1000) end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ct=1 end
+	local lp=Duel.GetLP(tp)
+	local max=math.min(ct,lp//1000)
+	local t={}
+	for i=1,max do
+		t[i]=i*1000
 	end
-	if chk==0 then
-		return uniq>0 and Duel.CheckLPCost(tp,uniq*1000)
-	end
-	local ct=Duel.AnnounceNumber(tp,table.unpack(aux.GetNumberTable(1,uniq)))
-	e:SetLabel(ct)
-	Duel.PayLPCost(tp,ct*1000)
+	local ac=Duel.AnnounceNumber(tp,table.unpack(t))
+	Duel.PayLPCost(tp,ac)
+	e:SetLabel(ac//1000)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
 	local ct=e:GetLabel()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ct,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,ct,ct,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,#sg,tp,0)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local ct=e:GetLabel()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<ct then return end
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,nil,e,tp)
-	local sg=aux.SelectUnselectGroup(g,e,tp,ct,ct,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
-	if #sg>0 then
-		if Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
-			--
+	local g=Duel.GetTargetCards(e)
+	if #g==0 or Duel.GetLocationCount(tp,LOCATION_MZONE)<#g or (#g>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)) then return end
+	local c=e:GetHandler()
+	for sc in g:Iter() do
+		if Duel.SpecialSummonStep(sc,0,tp,tp,false,false,POS_FACEUP) then
+			--Place it on the bottom of the Deck if it leaves the field
 			local e1=Effect.CreateEffect(c)
 			e1:SetDescription(3301)
 			e1:SetType(EFFECT_TYPE_SINGLE)
