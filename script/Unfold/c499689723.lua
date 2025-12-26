6e6e9634aa42f5559bda0a2b4351ab68be096b3b
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-26 17:05:45 +0700

    F

--- script/Unfold/c499689723.lua
+++ script/Unfold/c499689723.lua
@@ -75,11 +75,14 @@ end
 function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep==tp
 end
-function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil,e,tp) end
-end
 function s.desfilter(c,atk)
 	return c:IsFaceup() and c:IsAttackBelow(atk-1)
+end
+function s.cttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc,c:GetAttack()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,c:GetAttack()) end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
