a60509d23512f73fd4cf018b0006097b64e143ff
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-27 17:10:03 +0700

    Add

---
+++ script/Unfold/c499689699.lua
@@ -0,0 +1,88 @@
+--Aerolizer Brilliant Force
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro level
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SYNCHRO_LEVEL)
+	e1:SetValue(s.slevel)
+	c:RegisterEffect(e1)
+	--Grant Effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1,id)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.con)
+	e2:SetTarget(s.tg)
+	e2:SetOperation(s.op)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.eftg)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+	--Todeck
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TODECK)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCountLimit(1)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetCondition(s.rcon)
+	e4:SetTarget(s.rtg)
+	e4:SetOperation(s.rop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0xbf45}
+function s.slevel(e,c)
+	local lv=e:GetHandler():GetLevel()
+	return 1*65536+lv
+end
+function s.eftg(e,c)
+	local g=e:GetHandler()
+	return c:IsSetCard(0xbf45) 
+end
+function s.rcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function s.rtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeck() end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function s.rop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsAbleToDeck() then
+	Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
+end
+end
+function s.add(c)
+	return c:IsSpellTrap() and c:IsSetCard(0xbf45) and c:IsIsAbleToHand()
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.add,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,s.add,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+	if Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
+	Duel.ConfirmCards(1-tp,g)
+	Duel.Recover(tp,500,REASON_EFFECT)
+end
+end
+end
+function s.rcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function s.rtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeck() end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function s.rop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsAbleToDeck() then
+	Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
+end
+end
