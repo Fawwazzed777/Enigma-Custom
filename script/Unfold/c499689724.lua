a2e6ba8edd030c7ee76ffe98a502ba6f5d74d151
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-29 17:37:48 +0700

    Update c499689724.lua

--- script/Unfold/c499689724.lua
+++ script/Unfold/c499689724.lua
@@ -29,8 +29,8 @@ function s.cfilter(c,ft,tp)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,e,ft,tp) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,e,ft,tp)
+	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.cfilter,1,true,nil,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,true,nil,nil)
 	Duel.Release(g,REASON_COST)
 end
 function s.filter(c,e,tp)
