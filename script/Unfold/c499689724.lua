7908ffeb43894e0ce5788cdfb6e787a9f7b25d64
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-29 22:00:01 +0700

    F

--- script/Unfold/c499689724.lua
+++ script/Unfold/c499689724.lua
@@ -30,7 +30,6 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return ft>-1 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND|LOCATION_GRAVE,0,1,nil,e,tp)
 	and Duel.CheckReleaseGroupCost(tp,s.cfilter,1,true,nil,nil) end
 	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,true,nil,nil)
-	Duel.SetTargetCard(Duel.GetOperatedGroup():GetFirst())
 	Duel.Release(g,REASON_COST)
 	Duel.ShuffleHand(tp)
 end
