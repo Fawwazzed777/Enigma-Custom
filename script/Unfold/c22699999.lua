9f9a9d85de7d328bbee37e83c536d1675c7a2225
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-30 23:29:21 +0700

    Updates

--- script/Unfold/c22699999.lua
+++ script/Unfold/c22699999.lua
@@ -15,6 +15,17 @@ c:SetUniqueOnField(1,0,id)
 	e0:SetValue(ATTRIBUTE_DARK)
 	c:RegisterEffect(e0)
 	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.rcost)
+	e1:SetTarget(s.rtg)
+	e1:SetOperation(s.rop)
+	c:RegisterEffect(e1)
 end
 function s.ffilter1(c,fc,sumtype,tp)
 	return c:IsCode(16399999)
@@ -31,4 +42,30 @@ end
 function s.contactop(g,tp)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.SendtoDeck(g,nil,2,REASON_COST+REASON_MATERIAL)
+end
+function s.costfilter1(c)
+	return c:IsSetCard(0x993) and c:IsAbleToRemoveAsCost()
+end
+function s.rcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter1,tp,LOCATION_GRAVE,0,1,nil) end
+	local maxtc=Duel.GetTargetCount(nil,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter1,tp,LOCATION_GRAVE,0,1,maxtc,nil)
+	local cg=Duel.Remove(g,POS_FACEUP,REASON_COST)
+	e:SetLabel(cg)
+end
+function s.rtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,0,LOCATION_ONFIELD,1,nil) end
+end
+function s.rop(e,tp,eg,ep,ev,re,r,rp)
+	local rg=Duel.GetTargetCards(e)
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local rg=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,ct,0,0)
+	if #rg>0 then
+		Duel.HintSelection(rg)
+		Duel.SendtoHand(rg,nil,REASON_EFFECT)
+	end
 end
