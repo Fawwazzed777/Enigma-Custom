1f53eaf5551c631fa4a24d65ac4667a6b594208d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2025-12-30 23:35:24 +0700

    F

--- script/Unfold/c22699999.lua
+++ script/Unfold/c22699999.lua
@@ -29,7 +29,6 @@ c:SetUniqueOnField(1,0,id)
 	--recycle
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetCountLimit(1)
 	e2:SetRange(LOCATION_MZONE)
