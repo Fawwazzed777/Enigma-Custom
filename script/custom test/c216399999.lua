09c7e6683406bf8d90f80d0341c7bdaf2393d297
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-01 00:25:42 +0700

    Update c216399999.lua

--- script/custom test/c216399999.lua
+++ script/custom test/c216399999.lua
@@ -11,7 +11,8 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,{id,0})
 	e2:SetCost(s.ttcost)
@@ -28,13 +29,13 @@ function s.tgfilter(c,att)
 	return c:IsSetCard(0x994) and not c:IsAttribute(att) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
 end
 function s.ttcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil,tp) end
-	Duel.Remove(tp,POS_FACEUP,s.cfilter,1,1,REASON_COST,nil,tp)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,nil,tp)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 	e:SetLabel(Duel.GetOperatedGroup():GetFirst():GetAttribute())
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
 function s.top(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local tc=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e:GetLabel(),e,tp):GetFirst()
 	if tc and tc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
