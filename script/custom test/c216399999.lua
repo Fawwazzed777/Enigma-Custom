d209ce389df54a4b040a117d61ee53f7333a89d0
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-01 01:51:20 +0700

    Update c216399999.lua

--- script/custom test/c216399999.lua
+++ script/custom test/c216399999.lua
@@ -22,18 +22,17 @@ function s.initial_effect(c)
 end
 s.listed_series={0x994}
 function s.costfilter(c)
-	return c:IsSetCard(0x994) and c:IsMonster() and c:GetOriginalAttribute()~=att
-	and (c:IsFaceup() and c:IsLocation(LOCATION_MZONE) or c:IsLocation(LOCATION_HAND))
+	return c:IsSetCard(0x994) and c:IsMonster()
+		and ((c:IsFaceup() and c:IsLocation(LOCATION_MZONE)) or c:IsLocation(LOCATION_HAND))
 		and c:IsAbleToRemoveAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil)
-	and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,ATTRIBUTE_ALL)end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,1,1,nil)
-	local att=g:GetFirst():GetOriginalAttribute()
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-	e:SetLabel(att)
+	local tc=g:GetFirst()
+	e:SetLabel(tc:GetOriginalAttribute())
+	Duel.Remove(tc,POS_FACEUP,REASON_COST)
 end
 function s.spfilter(c,e,tp,att)
 	return c:IsSetCard(0x994) and c:IsMonster()
