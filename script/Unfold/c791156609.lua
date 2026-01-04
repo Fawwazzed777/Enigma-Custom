9d5e38d2c0fb7711548acfdca646471536cfc1c6
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-04 22:06:05 +0700

    Fix

--- script/Unfold/c791156609.lua
+++ script/Unfold/c791156609.lua
@@ -1,110 +1,100 @@
---Vandal Princess Devaty
+--Vandal Duo Ev Ida
 local s,id=GetID()
 function s.initial_effect(c)
-	--Vandal 
+	--Special Summon from hand + banish GY + gain ATK/DEF
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
-	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,{id,0})
-	e1:SetCondition(s.vcon)
-	e1:SetTarget(s.vtg)
-	e1:SetOperation(s.vop)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--
+	--Destroy 1 monster if other monster activated effect
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_ATKCHANGE)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_CHAINING)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.co)
-	e2:SetTarget(s.ta)
-	e2:SetOperation(s.op)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
-	--Check for activation on field
+	--Global check: register monsters that activated effects on field
 	aux.GlobalCheck(s,function()
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetCode(EVENT_CHAINING)
-		ge1:SetCondition(s.regcon)
-		ge1:SetOperation(s.regop1)
-		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_CHAIN_NEGATED)
-		ge2:SetCondition(s.regcon)
-		ge2:SetOperation(s.regop2)
-		Duel.RegisterEffect(ge2,0)
+		local ge=Effect.CreateEffect(c)
+		ge:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge:SetCode(EVENT_CHAINING)
+		ge:SetOperation(s.regop)
+		Duel.RegisterEffect(ge,0)
 	end)
 end
 s.listed_series={0x963}
-function s.vcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=re:GetHandler()
-	return rp==tp and re:IsActiveType(TYPE_MONSTER) and rc~=c 
+	if rc:IsOnField() and rc:IsType(TYPE_MONSTER) then
+		rc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	end
 end
-function s.vtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return rp==tp and re:IsActiveType(TYPE_MONSTER)
+		and re:GetHandler()~=c
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	if Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_GRAVE)
+	end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 then return end
+	--Optional banish opponent GY
+	if Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,1,nil)
+		if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)~=0 then
+		--Count other monsters that activated effect this turn
+		local ct=Duel.GetMatchingGroupCount(function(tc) return tc:GetFlagEffect(id)>0 and tc~=c end,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+		if ct>0 then
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(ct*200)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+			c:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			c:RegisterEffect(e2)
+end
+end
+end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return rp==tp and re:IsActiveType(TYPE_MONSTER)
+		and re:GetHandler()~=e:GetHandler()
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_MZONE)
 end
-function s.vop(e,tp,eg,ep,ev,re,r,rp)
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,e:GetHandler())
 	if #g>0 then
-		Duel.HintSelection(g)
 		Duel.Destroy(g,REASON_EFFECT)
 	end
-end
---
-function s.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandler():IsOnField()
-end
-function s.regop1(e,tp,eg,ep,ev,re,r,rp)
-	re:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-end
-function s.regop2(e,tp,eg,ep,ev,re,r,rp)
-	local ct=re:GetHandler():GetFlagEffect(id)
-	re:GetHandler():ResetFlagEffect(id)
-	for i=1,ct-1 do
-		re:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
-	end
-end
---
-function s.rfilter(c)
-	return c:IsFaceup() and c:GetFlagEffect(id)>0
-end
-function s.co(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.IsExistingMatchingCard(s.rfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler())
-end
-function s.ta(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) end
-	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,nil)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if c:IsFacedown() then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,1,nil)
-	if #g>0 then
-	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)~=0 then
-	local ct=Duel.GetMatchingGroupCount(s.rfilter,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())	
-	if ct>0 then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(100*ct)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
-		c:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		c:RegisterEffect(e2)
-end
-end
-end
 end
