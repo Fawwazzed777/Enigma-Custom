baa6c11106ea1037717c2b4ea53cd4729a741322
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-04 17:36:25 +0700

    Update c791156613.lua

--- script/Unfold/c791156613.lua
+++ script/Unfold/c791156613.lua
@@ -6,7 +6,8 @@ function s.initial_effect(c)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e0)
-	--All face-up monsters become LIGHT Fiend while "Devas" monster exists
+	c:EnableCounterPermit(COUNTER_VANDAL)
+	--All face-up monsters become LIGHT Fiend while "Devas" exists
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetRange(LOCATION_FZONE)
@@ -19,20 +20,20 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_CHANGE_RACE)
 	e2:SetValue(RACE_FIEND)
 	c:RegisterEffect(e2)
-	--ATK gain for Devas Xyz with Vandal material
+	--ATK/DEF gain for Devas Xyz with Vandal material (your turn only)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetTargetRange(LOCATION_MZONE,0)
 	e3:SetTarget(s.devas_xyz)
-	e3:SetValue(s.val)
+	e3:SetValue(s.atkval)
 	c:RegisterEffect(e3)
-	--DEF gain
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_UPDATE_DEFENSE)
+	e4:SetValue(s.atkval)
 	c:RegisterEffect(e4)
-	--Opponent monsters lose ATK on each chain activation
+	--Add Vandal Counter on each chain activation
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e5:SetRange(LOCATION_FZONE)
@@ -40,10 +41,20 @@ function s.initial_effect(c)
 	e5:SetCondition(s.chaincon)
 	e5:SetOperation(s.chainop)
 	c:RegisterEffect(e5)
+	--Opponent monsters lose ATK based on Vandal Counters
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetRange(LOCATION_FZONE)
+	e6:SetCode(EFFECT_UPDATE_ATTACK)
+	e6:SetTargetRange(0,LOCATION_MZONE)
+	e6:SetValue(s.debuff)
+	c:RegisterEffect(e6)
 end
+s.listed_series={0x765,0x963}
 function s.devas_exist(e)
-	return Duel.IsExistingMatchingCard(function(c) return c:IsFaceup() 
-	and c:IsSetCard(0x765) end,e:GetHandlerPlayer(),LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	return Duel.IsExistingMatchingCard(function(c) return 
+	c:IsFaceup() and c:IsSetCard(0x765) end,
+	e:GetHandlerPlayer(),LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
 function s.vandal_mat(c)
 	return c:IsSetCard(0x963)
@@ -55,42 +66,33 @@ function s.devas_xyz(e,c)
 		and c:GetOverlayGroup():IsExists(s.vandal_mat,1,nil)
 		and Duel.GetTurnPlayer()==c:GetControler()
 end
-function s.light()
+function s.light_fiend_count()
 	return Duel.GetMatchingGroupCount(
 		function(c)
 			return c:IsFaceup()
 				and c:IsAttribute(ATTRIBUTE_LIGHT)
 				and c:IsRace(RACE_FIEND)
-		end,0,LOCATION_MZONE,LOCATION_MZONE,nil)
+		end,
+		0,LOCATION_MZONE,LOCATION_MZONE,nil)
 end
-function s.val(e,c)
-	return s.light()*200
-end
-function s.vfilter(c)
-	return c:IsFaceup()and c:IsType(TYPE_XYZ) and c:IsSetCard(0x963)
+function s.atkval(e,c)
+	return s.light_fiend_count()*300
 end
 function s.vandal_xyz_exist(tp)
-	return Duel.IsExistingMatchingCard(s.vfilter,tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(
+		function(c)
+			return c:IsFaceup()
+				and c:IsType(TYPE_XYZ)
+				and c:IsSetCard(0x963)
+		end,
+		tp,LOCATION_MZONE,0,1,nil)
 end
 function s.chaincon(e,tp,eg,ep,ev,re,r,rp)
 	return rp~=tp and s.vandal_xyz_exist(tp)
-		and Duel.GetFlagEffect(tp,id)==0
-end
-function s.oppc(c,e,tp) 
-	return c:IsFaceup() and c:IsControler(1-tp) and (c:HasNonZeroAttack() or c:HasNonZeroDefense())
 end
 function s.chainop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,id,RESET_EVENT+RESETS_STANDARD,0,1)
-	local g=Duel.GetMatchingGroup(s.oppc,tp,0,LOCATION_MZONE,nil)
-	for tc in aux.Next(g) do
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(-100)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		local e2=e1:Clone()
-		e2:SetCode(EFFECT_UPDATE_DEFENSE)
-		tc:RegisterEffect(e2)
-	end
+	e:GetHandler():AddCounter(COUNTER_VANDAL,1)
+end
+function s.debuff(e,c)
+	return -100*e:GetHandler():GetCounter(COUNTER_VANDAL)
 end
