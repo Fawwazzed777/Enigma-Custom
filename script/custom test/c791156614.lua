12d289aa4af036bd94433011de2e15f9d13af322
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-05 18:35:23 +0700

    F

--- script/custom test/c791156614.lua
+++ script/custom test/c791156614.lua
@@ -102,10 +102,9 @@ function s.reltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,0,LOCATION_MZONE)
 end
 function s.relop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DETACH)
-	local xc=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil):GetFirst()
+	local xc=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
 	if not xc then return end
-	if xc:RemoveOverlayCard(tp,1,1,REASON_EFFECT)==0 then return end
+	if xc:RemoveOverlayCard(tp,1,0,1,1,REASON_EFFECT)==0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsReleasable,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	if #g>0 then
