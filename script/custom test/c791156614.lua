a4b55dbd00e9702b7e3efb1c211b3ff78c55d4ad
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-05 18:58:33 +0700

    F

--- script/custom test/c791156614.lua
+++ script/custom test/c791156614.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	--Xyz Summon
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.ffilter),7,2,nil,nil,Xyz.InfiniteMats)
 	c:EnableReviveLimit()
-	--On Xyz Summon: SS Devas, attach self if Xyz
+	--On Xyz Summon: SS Devas, attach own material if Xyz
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -16,31 +16,17 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--If opponent activates monster effect while this card is material
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_NEGATE)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_CHAINING)
-	e2:SetRange(LOCATION_OVERLAY)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.matcon)
-	e2:SetTarget(s.mattg)
-	e2:SetOperation(s.matop)
-	c:RegisterEffect(e2)
 	--Detach 1 material, then Tribute 1 monster
-	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_RELEASE)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,{id,2})
-	e3:SetTarget(s.reltg)
-	e3:SetOperation(s.relop)
-	c:RegisterEffect(e3)
-
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_RELEASE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,2})
+	e2:SetTarget(s.reltg)
+	e2:SetOperation(s.relop)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x765,0x963}
 function s.ffilter(c)
@@ -62,32 +48,12 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp):GetFirst()
 	if not tc then return end
 	if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)==0 then return end
-	--If Xyz monster, attach this card and its materials
+	--If Xyz monster, attach this card materials
 	if tc:IsType(TYPE_XYZ) and c:IsRelateToEffect(e) then
 		local og=c:GetOverlayGroup()
 		if #og>0 then
 			Duel.Overlay(tc,og)
 		end
-		Duel.Overlay(tc,Group.FromCards(c))
-	end
-end
--------------------------------------------------
---Material effect
-function s.matcon(e,tp,eg,ep,ev,re,r,rp)
-	return rp==1-tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
-end
-function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-end
-function s.matop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local rc=re:GetHandler()
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 then return end
-	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
-		Duel.Overlay(c,Group.FromCards(rc))
 	end
 end
 function s.xyzfilter(c)
