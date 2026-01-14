a67ad3185404c29f19f74e87ad85910d493b249a
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-14 07:16:28 +0700

    F

---
+++ script/custom test/c217399999.lua
@@ -0,0 +1,79 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,s.ffilter1,s.ffilter2)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+	--Shuffle banished "Eternity Tech" to debuff
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.atktg)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+end
+s.listed_names={16599999}
+s.listed_series={0x994} 
+function s.ffilter1(c,fc,sumtype,tp)
+	return c:IsCode(16599999) 
+end
+function s.ffilter2(c,fc,sumtype,tp)
+	return c:IsRace(RACE_CYBERSE) and c:IsType(TYPE_XYZ) 
+end
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(function(c) return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckOrExtraAsCost() end,tp,LOCATION_ONFIELD,0,nil)
+end
+function s.contactop(g,tp)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.SendtoDeck(g,nil,2,REASON_COST+REASON_MATERIAL)
+end
+function s.rmfilter(c)
+	return c:IsSetCard(0x994) and c:IsAbleToDeck()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsMonster,tp,0,LOCATION_MZONE,1,nil) and
+	Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_REMOVED,0,1,nil) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local maxtc=Duel.GetTargetCount(nil,tp,0,LOCATION_ONFIELD,nil)
+	local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_REMOVED,0,1,maxtc,nil)
+	local ct=Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	if ct==0 then return end
+	local val=ct*500
+	local og=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	for tc in aux.Next(og) do
+		--ATK down
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(-val)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		--If ATK becomes 0
+		if tc:GetAttack()-val<=0 then
+			--Negate effects
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_DISABLE)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+			local e3=e2:Clone()
+			e3:SetCode(EFFECT_DISABLE_EFFECT)
+			tc:RegisterEffect(e3)
+			--Cannot be battle target
+			local e4=Effect.CreateEffect(c)
+			e4:SetType(EFFECT_TYPE_SINGLE)
+			e4:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+			e4:SetValue(1)
+			e4:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e4)
+		end
+	end
+end
