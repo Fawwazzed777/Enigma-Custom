7ffcd2fe8a0d356dac17dd459967e23b537d90a8
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-14 09:56:48 +0700

    F

--- script/custom test/c217399999.lua
+++ script/custom test/c217399999.lua
@@ -16,6 +16,26 @@ function s.initial_effect(c)
 	e1:SetTarget(s.atktg)
 	e1:SetOperation(s.atkop)
 	c:RegisterEffect(e1)
+	--PZ Effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.dpentg)
+	e2:SetOperation(s.dpenop)
+	c:RegisterEffect(e2)
+	--To PZ
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetCondition(s.pencon)
+	e3:SetTarget(s.pentg)
+	e3:SetOperation(s.penop)
+	c:RegisterEffect(e3)
 end
 s.listed_names={16599999}
 s.listed_series={0x993} 
@@ -48,7 +68,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_REMOVED,0,1,99,nil)
 	local ct=Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
 	if ct==0 then return end
-	local val=ct*500
+	local val=ct*600
 	local og=Duel.GetMatchingGroup(s.afilter,tp,0,LOCATION_MZONE,nil)
 	for tc in aux.Next(og) do
 	local prev_atk=tc:GetAttack()
@@ -96,4 +116,42 @@ function s.dirfilter(c)
 end
 function s.dirtg(e,c)
 	return not Duel.IsExistingMatchingCard(s.dirfilter,c:GetControler(),0,LOCATION_MZONE,1,nil)
+end
+function s.pendfilter(c)
+	return c:IsMonster() and c:HasLevel() and c:IsFaceup()
+end
+function s.dpentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local g=Duel.GetMatchingGroup(s.pendfilter,tp,LOCATION_MZONE,0,nil)
+		return aux.SelectUnselectGroup(g,e,tp,2,2,function(g) return g:GetSum(Card.GetLevel)==7 end,0) 
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,2,tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_PZONE)
+end
+
+function s.dpenop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.pendfilter,tp,LOCATION_MZONE,0,nil)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,function(g) return g:GetSum(Card.GetLevel)==7 end,1,tp,HINTMSG_DESTROY)
+	if #sg~=2 then return end
+	if Duel.Destroy(sg,REASON_EFFECT)==2 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.pencon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_MZONE) and c:IsFaceup()
+end
+function s.pentg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckPendulumZones(tp) end
+end
+function s.penop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckPendulumZones(tp) then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	end
 end
