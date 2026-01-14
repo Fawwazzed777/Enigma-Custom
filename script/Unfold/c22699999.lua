0c71b4bad5ebc156e611c1ce21aaa50fc189236b
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-14 21:16:09 +0700

    F

--- script/Unfold/c22699999.lua
+++ script/Unfold/c22699999.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 c:SetUniqueOnField(1,0,id)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,s.ffilter1,s.ffilter2,s.ffilter3)
+	Fusion.AddProcMix(c,true,true,s.ffilter1,s.ffilter2,s.ffilter3,s.ffilter4)
 	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
 	--
 	local e0=Effect.CreateEffect(c)
@@ -44,6 +44,9 @@ function s.ffilter2(c,fc,sumtype,tp)
 end
 function s.ffilter3(c,fc,sumtype,tp)
 	return c:IsSetCard(0x994) and c:IsAttribute(ATTRIBUTE_DARK) 
+end
+function s.ffilter4(c,fc,sumtype,tp)
+	return c:IsCode(14899999) and c:IsLocation(LOCATION_FZONE)
 end
 function s.contactfil(tp)
 	return Duel.GetMatchingGroup(function(c) return c:IsType(TYPE_MONSTER) and c:IsAbleToDeckOrExtraAsCost() end,tp,LOCATION_ONFIELD,0,nil)
