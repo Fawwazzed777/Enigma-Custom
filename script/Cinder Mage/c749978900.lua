e4c9a6d9b2ffbab3e4886dceef0d7d856a5a9e48
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-01-27 15:11:36 +0700

    F

--- script/Cinder Mage/c749978900.lua
+++ script/Cinder Mage/c749978900.lua
@@ -1,7 +1,7 @@
---Cinder Mage Recaller Falina
+--Pyrorixis Mage Falina
 local s,id=GetID()
 function s.initial_effect(c)
-	--Copy Spell from GY
+	--Copy "Pyrorixis" Spell from GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TODECK)
@@ -12,36 +12,73 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	--Special Summon from hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.hsptg)
+	e2:SetOperation(s.hspop)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x7f3}
 function s.filter(c)
-	return c:IsSetCard(0x7f3) and c:IsType(TYPE_SPELL) and c:IsAbleToDeckAsCost()
+	return c:IsSetCard(0x7f3) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToDeckAsCost()
 	and c:CheckActivateEffect(false,true,false)~=nil
 end
 function s.ccost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
-	local te=g:GetFirst():CheckActivateEffect(false,true,true)
-	s[Duel.GetCurrentChain()]=te
-	Duel.SendtoDeck(g,nil,2,REASON_COST)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
+	e:SetLabelObject(tc)
+	Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_COST)
 end
+
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local te=s[Duel.GetCurrentChain()]
-	if chkc then
-		local tg=te:GetTarget()
-		return tg(e,tp,eg,ep,ev,re,r,rp,0,true)
+	local tc=e:GetLabelObject()
+	if not tc then return false end
+	local te=tc:GetActivateEffect()
+	if not te then return false end
+	local tg=te:GetTarget()
+	if chk==0 then
+		return tg==nil or tg(e,tp,eg,ep,ev,re,r,rp,0)
 	end
-	if chk==0 then return true end
-	if not te then return end
 	e:SetCategory(te:GetCategory())
 	e:SetProperty(te:GetProperty())
-	local tg=te:GetTarget()
 	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
 end
+
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local te=s[Duel.GetCurrentChain()]
+	local tc=e:GetLabelObject()
+	if not tc then return end
+	local te=tc:GetActivateEffect()
 	if not te then return end
 	local op=te:GetOperation()
-	if op then op(e,tp,eg,ep,ev,re,r,rp) end
+	if op then
+		op(e,tp,eg,ep,ev,re,r,rp)
+	end
+end
+--
+function s.hspfilter(c,e,tp)
+	return c:IsSetCard(0x7f3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.fire(c)
+	return c:IsSetCard(0x7f3) and c:IsType(TYPE_SPELL)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.hspfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.hspfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 
+		and Duel.IsExistingMatchingCard(s.fire,tp,LOCATION_GRAVE,0,1,nil) then
+		Duel.BreakEffect()
+		Duel.Damage(1-tp,200,REASON_EFFECT)
+	end
 end
