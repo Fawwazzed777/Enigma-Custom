40dae135f474c75ff9d73dd1f887e13190e19e73
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-05 10:27:30 +0700

    F

--- script/Pyrorixis/c749978904.lua
+++ script/Pyrorixis/c749978904.lua
@@ -25,21 +25,19 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SendtoGrave(g,REASON_COST)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local exc=nil
-	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then exc=e:GetHandler() end
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,exc) end
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
     Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,PLAYER_ALL,LOCATION_MZONE)
 end
 function s.spfilter(c,e,tp)
     return c:IsSetCard(0x7f3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local exc=nil
-	if e:IsHasType(EFFECT_TYPE_ACTIVATE) then exc=e:GetHandler() end	
-    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,exc)
+    local c=e:GetHandler()
+    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
     for tc in aux.Next(g) do
         if not tc:IsSetCard(0x7f3) then
-            local e1=Effect.CreateEffect(e:GetHandler())
+            Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+            local e1=Effect.CreateEffect(c)
             e1:SetType(EFFECT_TYPE_SINGLE)
             e1:SetCode(EFFECT_DISABLE)
             e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
@@ -49,25 +47,35 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
             tc:RegisterEffect(e2)
         end
     end
-    local ec=e:GetHandler()
-	if ec:IsType(TYPE_MONSTER) or not e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+    local is_monster= e:GetHandler():IsType(TYPE_MONSTER)
+    local is_recast= not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+
+    if is_monster or is_recast then
         if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
             and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-            and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+            and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then            
             Duel.BreakEffect()
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
             local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
             if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
-                Duel.Recover(tp,sc:GetLevel()*300,REASON_EFFECT)
-                -- FIRE Lock
+                local lv=sc:GetLevel()
+                if lv>0 then
+                    Duel.Recover(tp,lv*300,REASON_EFFECT)
+                end                
+                -- Lock FIRE only
                 local e3=Effect.CreateEffect(c)
                 e3:SetType(EFFECT_TYPE_FIELD)
                 e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
                 e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+                e3:SetDescription(aux.Stringid(id,1))
                 e3:SetTargetRange(1,0)
-                e3:SetTarget(function(e,c) return not c:IsAttribute(ATTRIBUTE_FIRE) end)
+                e3:SetTarget(s.splimit)
                 e3:SetReset(RESET_PHASE+PHASE_END)
                 Duel.RegisterEffect(e3,tp)
             end
         end
     end
+end
+function s.splimit(e,c)
+    return not c:IsAttribute(ATTRIBUTE_FIRE)
 end
