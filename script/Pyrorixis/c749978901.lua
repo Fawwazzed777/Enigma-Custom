028e8c2cbbdc17da73791bb6f90defb215b901a1
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-05 11:35:35 +0700

    Update c749978901.lua

--- script/Pyrorixis/c749978901.lua
+++ script/Pyrorixis/c749978901.lua
@@ -25,21 +25,23 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	local is_apply= not e:IsHasType(EFFECT_TYPE_ACTIVATE)
-	local rc= re and re:GetHandler()
-	local is_pyrorixis_monster= rc and rc:IsSetCard(0x7f3) and rc:IsType(TYPE_MONSTER)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
-	if #g>0 then
-		tc=g:Select(tp,1,1,nil):GetFirst()
+	local is_apply = not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+	local rc = re and re:GetHandler()
+	local is_pyrorixis_monster = rc and rc:IsSetCard(0x7f3) and rc:IsType(TYPE_MONSTER)
+	if is_apply then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+		if #g>0 then
+			tc=g:Select(tp,1,1,nil):GetFirst()
+		end
 	end
-end
-	if tc and is_apply then
-	if Duel.Destroy(tc,REASON_EFFECT)>0 then
-		--Bonus Recast
-		if is_apply and is_pyrorixis_monster then
-			Duel.BreakEffect()
-			Duel.Damage(1-tp,800,REASON_EFFECT)
+	if tc and (is_apply or tc:IsRelateToEffect(e)) then
+		if Duel.Destroy(tc,REASON_EFFECT)>0 then
+			--RECAST
+			if is_apply and is_pyrorixis_monster then
+				Duel.BreakEffect()
+				Duel.Damage(1-tp,800,REASON_EFFECT)
+			end
 		end
 	end
 end
