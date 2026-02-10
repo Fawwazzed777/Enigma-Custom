6c882c39df41165425d257aa7199ef20d3abc81d
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-10 12:09:36 +0700

    Update c749978904.lua

--- script/Pyrorixis/c749978904.lua
+++ script/Pyrorixis/c749978904.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     --Activate
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_DISABLE+CATEGORY_SPECIAL_SUMMON+CATEGORY_RECOVER)
+    e1:SetCategory(CATEGORY_DISABLE+CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
@@ -21,28 +21,31 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
         and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,e:GetHandler()) end
     Duel.PayLPCost(tp,1000)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,e:GetHandler())
+    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,e:GetHandler())
     Duel.SendtoGrave(g,REASON_COST)
 end
 function s.negfilter(c)
-    return c:IsFaceup() and not c:IsDisabled() and not c:IsSetCard(0x7f3)
+    return c:IsFaceup() and not c:IsDisabled()
 end
 function s.spfilter(c,e,tp)
     return c:IsSetCard(0x7f3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.negfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) then
-        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
-    end
+    if chk==0 then 
+        local b1=Duel.IsExistingMatchingCard(s.negfilter,tp,0,LOCATION_MZONE,1,nil)
+        local b2=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+                 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+        return b1 or b2 
+    end    
+    local g=Duel.GetMatchingGroup(s.negfilter,tp,0,LOCATION_MZONE,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local g=Duel.GetMatchingGroup(s.negfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)   
-    --Main Effect
-    for tc in aux.Next(g) do
-        if not tc:IsSetCard(0x7f3) then
+    local g=Duel.GetMatchingGroup(s.negfilter,tp,0,LOCATION_MZONE,nil)  
+    if #g>0 then
+        for tc in aux.Next(g) do
             local e1=Effect.CreateEffect(c)
             e1:SetType(EFFECT_TYPE_SINGLE)
             e1:SetCode(EFFECT_DISABLE)
@@ -53,12 +56,11 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
             tc:RegisterEffect(e2)
         end
     end
-
-    -- RECAST
+    --RECAST (Special Summon)
     if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
         and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
         and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then            
-        Duel.BreakEffect()
+        if #g>0 then Duel.BreakEffect() end        
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
         local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
         if #sc>0 then
