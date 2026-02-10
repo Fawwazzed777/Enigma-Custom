e7c7c2e6a24be047b21fad043e2e2d0dc2cf4c64
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-10 12:01:50 +0700

    Update c749978904.lua

--- script/Pyrorixis/c749978904.lua
+++ script/Pyrorixis/c749978904.lua
@@ -21,57 +21,48 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
         and Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,e:GetHandler()) end
     Duel.PayLPCost(tp,1000)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-    local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,e:GetHandler())
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_ONFIELD,0,1,1,e:GetHandler())
     Duel.SendtoGrave(g,REASON_COST)
 end
-function s.negfilter(c,e)
-    return not c:IsSetCard(0x7f3) and c:IsNegatableMonster()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.negfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-    Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,PLAYER_ALL,LOCATION_MZONE)
+function s.negfilter(c)
+    return c:IsFaceup() and not c:IsDisabled() and not c:IsSetCard(0x7f3)
 end
 function s.spfilter(c,e,tp)
     return c:IsSetCard(0x7f3) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=Duel.GetMatchingGroup(s.negfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	for tc in aux.Next(g) do
-		if not tc:IsSetCard(0x7f3) then
-			local e1=Effect.CreateEffect(c)
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e1)
-			local e2=e1:Clone()
-			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			tc:RegisterEffect(e2)
-		end
-	end
-
-	--RECAST
-	if not e:IsHasType(EFFECT_TYPE_ACTIVATE) then
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-			and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then			
-			Duel.BreakEffect()
-			local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp):GetFirst()
-			if sc and Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)>0 then
-				Duel.Recover(tp,sc:GetLevel()*300,REASON_EFFECT)
-				-- Lock FIRE
-				local e3=Effect.CreateEffect(c)
-				e3:SetType(EFFECT_TYPE_FIELD)
-				e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-				e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-				e3:SetTargetRange(1,0)
-				e3:SetTarget(s.splimit)
-				e3:SetReset(RESET_PHASE+PHASE_END)
-				Duel.RegisterEffect(e3,tp)
-			end
-		end
-	end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.negfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) then
+        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+    end
 end
-function s.splimit(e,c)
-    return not c:IsAttribute(ATTRIBUTE_FIRE)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local g=Duel.GetMatchingGroup(s.negfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)   
+    --Main Effect
+    for tc in aux.Next(g) do
+        if not tc:IsSetCard(0x7f3) then
+            local e1=Effect.CreateEffect(c)
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_DISABLE)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+            tc:RegisterEffect(e1)
+            local e2=e1:Clone()
+            e2:SetCode(EFFECT_DISABLE_EFFECT)
+            tc:RegisterEffect(e2)
+        end
+    end
+
+    -- RECAST
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+        and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then            
+        Duel.BreakEffect()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+        if #sc>0 then
+            Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
 end
