889dcce036d308bb73c5cb9d211ec035c81bcf0b
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-10 15:27:43 +0700

    F

--- script/Pyrorixis/c749978906.lua
+++ script/Pyrorixis/c749978906.lua
@@ -19,25 +19,14 @@ end
 function s.negfilter(c)
 	return c:IsFaceup() and not c:IsDisabled()
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and s.negfilter(chkc) end
-	-- RECAST CHECK
-	local is_recast = re and re:GetHandler():IsSetCard(0x7f3) and re:GetHandler():IsMonster()
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then 
-		local b1 = Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
-		if is_recast then
-			return b1 and Duel.IsExistingTarget(s.negfilter,tp,0,LOCATION_ONFIELD,1,nil)
-		end
-		return b1
+		return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	local is_recast = re and re:GetHandler():IsSetCard(0x7f3) and re:GetHandler():IsMonster()
 	if is_recast then
-		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-		local g=Duel.SelectTarget(tp,s.negfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
-		Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
-	else
-		e:SetProperty(0)
+		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DISABLE)
 	end
 end
 
@@ -49,25 +38,30 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			Duel.ConfirmCards(1-tp,g)
 		end
 	end
-	--RECAST:Pyrorixis monster
+	--RECAST EFFECT
 	if re and re:GetHandler():IsSetCard(0x7f3) and re:GetHandler():IsMonster() then
-		local tc=Duel.GetFirstTarget()
-		if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-			Duel.BreakEffect()		
-			-- Negate Effect
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-			tc:RegisterEffect(e1)			
-			-- Disable Effect Activation
-			local e2=e1:Clone()
-			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			tc:RegisterEffect(e2)
-			local e3=e1:Clone()
-			e3:SetCode(EFFECT_CANNOT_TRIGGER)
-			tc:RegisterEffect(e3)						
-			Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
+		local sg=Duel.GetMatchingGroup(s.negfilter,tp,0,LOCATION_ONFIELD,nil)
+		if #sg>0 then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISABLE)
+			local tc=sg:Select(tp,1,1,nil):GetFirst()
+			if tc then
+				--Negate Effect
+				local e1=Effect.CreateEffect(e:GetHandler())
+				e1:SetType(EFFECT_TYPE_SINGLE)
+				e1:SetCode(EFFECT_DISABLE)
+				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+				tc:RegisterEffect(e1)
+				--Disable Effect Activation
+				local e2=e1:Clone()
+				e2:SetCode(EFFECT_DISABLE_EFFECT)
+				tc:RegisterEffect(e2)
+				--Cannot Trigger
+				local e3=e1:Clone()
+				e3:SetCode(EFFECT_CANNOT_TRIGGER)
+				tc:RegisterEffect(e3)				
+				Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
+			end
 		end
 	end
 end
