0fa95279fcf4157a89b6a410dc021f015e01dfb4
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-10 14:51:15 +0700

    F

--- script/Pyrorixis/c749978906.lua
+++ script/Pyrorixis/c749978906.lua
@@ -19,26 +19,20 @@ end
 function s.negfilter(c)
 	return c:IsFaceup() and not c:IsDisabled()
 end
-function s.check_recast(e)
-	local re=e:GetOwnerEffect()
-	if re and re:GetHandler():IsSetCard(0x7f3) and re:GetHandler():IsMonster() then return true end
-	local ev=Duel.GetCurrentChain()
-	if ev>0 then
-		local te=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_EFFECT)
-		local tc=te:GetHandler()
-		return tc:IsSetCard(0x7f3) and tc:IsMonster()
-	end
-	return not e:IsHasType(EFFECT_TYPE_ACTIVATE)
-end
-
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and s.negfilter(chkc) end	
-	local is_recast = s.check_recast(e)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end	
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and s.negfilter(chkc) end
+	local is_recast = not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+	if chk==0 then 
+		local b1 = Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil)
+		if is_recast then
+			return b1 and Duel.IsExistingTarget(s.negfilter,tp,0,LOCATION_ONFIELD,1,nil)
+		end
+		return b1
+	end	
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)	
 	if is_recast then
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DISABLE)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
 		local g=Duel.SelectTarget(tp,s.negfilter,tp,0,LOCATION_ONFIELD,1,1,nil)
 		Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
 	else
@@ -51,28 +45,30 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+		if Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+			Duel.ConfirmCards(1-tp,g)
+		end
 	end
-	--RECAST (Negate & Activation Lock)
+	local is_apply = not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+	--RECAST: Negate & Activation Lock
+	if is_apply and re:GetHandler():IsMonster() and re:GetHandler():IsSetCard(0x7f3) then
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.BreakEffect()
-		Duel.NegateRelatedChain(tc,RESET_TURN_SET)	
-		--Negate Effect
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
+		Duel.BreakEffect()		
+		-- Negate Effect
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-		--Disable Effect Activation
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)		
+		-- Disable Effect Activation
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
-		tc:RegisterEffect(e2)
-		--Cannot Activate Effects
+		tc:RegisterEffect(e2)		
+		-- Cannot Trigger
 		local e3=e1:Clone()
 		e3:SetCode(EFFECT_CANNOT_TRIGGER)
-		tc:RegisterEffect(e3)		
-		Duel.Hint(HINT_OPSELECTED,1-tp,aux.Stringid(id,1))
+		tc:RegisterEffect(e3)
+		end
 	end
 end
