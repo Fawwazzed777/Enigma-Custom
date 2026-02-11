092213406091a41bb05675b9406b64705ef8cff3
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-11 10:33:18 +0700

    Update c640146362.lua

--- script/custom test/c640146362.lua
+++ script/custom test/c640146362.lua
@@ -85,7 +85,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		--efek: [0] Special Summon or [1] Debuff ATK/DEF
 		local opt=Duel.SelectOption(tp, aux.Stringid(code,0), aux.Stringid(code,4))   
 		if opt==0 then
-        --Efek 1: Special Summon 1 "Enigmation" from GY
+        --efek: [1] Special Summon 1 "Enigmation" from GY
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
         local sg=Duel.SelectTarget(tp,s.spectral,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
         if #sg>0 then
