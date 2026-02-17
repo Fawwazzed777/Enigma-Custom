1acffb86af28145384d12cdc2fb828c337c40521
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-17 09:29:31 +0700

    Update user_cards_specific_functions.lua

--- script/user_cards_specific_functions.lua
+++ script/user_cards_specific_functions.lua
@@ -2,13 +2,6 @@
 function Auxiliary.DarkLightFLimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(17106529)
 end
---NOTE: Use this 'Duel.LoadScript("user_cards_specific_functions.lua")' in every script to connect all function on this script
 
-if not GenerateEffect then
-	GenerateEffect={}
-	--Additional ATTRIBUTE
-	ATTRIBUTE_RADIANT= 0x100
 
-	--Additional Types
-	RACE_VIRTUOUS	= 0x100000000
-	end
+
