e76fe3fd5934a4eef2f3b83be21b76ebe4d00f7f
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-18 12:55:06 +0700

    Fix

--- script/Unfold/c99911231.lua
+++ script/Unfold/c99911231.lua
@@ -23,11 +23,12 @@ function s.initial_effect(c)
     e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
     e2:SetCode(EVENT_REMOVE)
     e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.atkcon)
     e2:SetTarget(s.atktg)
     e2:SetOperation(s.atkop)
     c:RegisterEffect(e2)
 end
-s.listed_series={0x344}
+s.listed_series={0x145,0x344}
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(Card.IsSetCard,1,nil,0x344)
 end
@@ -44,28 +45,34 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
         Duel.Destroy(g,REASON_EFFECT)
     end
 end
+function s.enigma(c)
+	return (c:IsSetCard(0x145) or c:IsSetCard(0x344))
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(s.enigma),tp,LOCATION_REMOVED,0,1,e:GetHandler())
+end
 function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
     if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
     if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
     local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,1000)
 end
 function s.atkop(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetFirstTarget()
 	local atk=tc:GetAttack()
     if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-        -- Debuff ATK
+        --Debuff ATK
         local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetType(EFFECT_TYPE_SINGLE)
         e1:SetCode(EFFECT_UPDATE_ATTACK)
-        e1:SetValue(-1000)
+        e1:SetValue(-2000)
         e1:SetReset(RESET_EVENT+RESETS_STANDARD)
         tc:RegisterEffect(e1)
         --Gain 500
         if not tc:IsImmuneToEffect(e1) then
             Duel.BreakEffect()
-            Duel.Recover(tp,500,REASON_EFFECT)
+            Duel.Recover(tp,1000,REASON_EFFECT)
 			if atk~=0 and tc:IsAttack(0) then 
 			Duel.BreakEffect()
 			Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
