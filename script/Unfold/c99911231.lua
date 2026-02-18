4f1a8ed171c439b1bd1e31cb51a38d2bedb0243c
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-18 07:47:07 +0700

    Updates

---
+++ script/Unfold/c99911231.lua
@@ -0,0 +1,74 @@
+--Enigmation Lord - Noir Valkyrie
+local s,id=GetID()
+function s.initial_effect(c)
+    --Reveal from hand & Destroy
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_REMOVE)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,{id,0})
+    e1:SetCost(Cost.AND(Cost.SelfReveal,Cost.PayLP(1000)))
+	e1:SetCondition(s.descon)
+    e1:SetTarget(s.destg)
+    e1:SetOperation(s.desop)
+    c:RegisterEffect(e1)
+    --Banish effect (Debuff & LP Gain)
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_RECOVER)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e2:SetCode(EVENT_REMOVE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetTarget(s.atktg)
+    e2:SetOperation(s.atkop)
+    c:RegisterEffect(e2)
+end
+s.listed_series={0x344}
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsSetCard,1,nil,0x344)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+    if chk==0 then return #g>0 end
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+    local g=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    if #g>0 then
+        Duel.HintSelection(g)
+        Duel.Destroy(g,REASON_EFFECT)
+    end
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+    if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATKDEF)
+    local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+	local atk=tc:GetAttack()
+    if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        -- Debuff ATK
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(-1000)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1)
+        --Gain 500
+        if not tc:IsImmuneToEffect(e1) then
+            Duel.BreakEffect()
+            Duel.Recover(tp,500,REASON_EFFECT)
+			if atk~=0 and tc:IsAttack(0) then 
+			Duel.BreakEffect()
+			Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+        end
+    end
+end
