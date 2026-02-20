776d449b1808361f119930e0c14b9641c0ded8a8
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-21 06:07:11 +0700

    Update
    Enigmation/Phantasm Support

---
+++ script/Unfold/c779797891.lua
@@ -0,0 +1,110 @@
+--Enigmation - Phantasm Call
+local s,id=GetID()
+function s.initial_effect(c)
+    --Special Summon from Deck
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,{id,0})
+    e1:SetCost(s.spcost)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
+    --Recycle banished monster
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,1))
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetCondition(aux.exccon)
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.thtg)
+    e2:SetOperation(s.thop)
+    c:RegisterEffect(e2)
+end
+s.listed_series={0x344,0x145}
+function s.costfilter(c)
+    return c:IsSetCard(0x344) and (c:IsDiscardable() or (c:IsLocation(LOCATION_GRAVE) and c:IsAbleToRemoveAsCost()))
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then 
+        local res1=Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_HAND,0,1,nil,0x344) 
+		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		
+        local res2=Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_GRAVE,0,1,nil,0x344) 
+		and Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,1,nil)
+        return res1 or res2
+    end   
+    local op=0
+    local b1=Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_HAND,0,1,nil,0x344) 
+	and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+	
+    local b2=Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_GRAVE,0,1,nil,0x344) 
+	and Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,1,nil)
+    if b1 and b2 then
+        op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3)) -- 2: Discard, 3: Banish
+    elseif b1 then
+        op=Duel.SelectOption(tp,aux.Stringid(id,2))
+    else
+        op=Duel.SelectOption(tp,aux.Stringid(id,3))+1
+    end
+    
+    if op==0 then
+        Duel.DiscardHand(tp,Card.IsSetCard,1,1,REASON_COST+REASON_DISCARD,nil,0x344)
+    else
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+        local g=Duel.SelectMatchingCard(tp,Card.IsSetCard,tp,LOCATION_GRAVE,0,1,1,nil,0x344)
+        Duel.Remove(g,POS_FACEUP,REASON_COST)
+    end
+end
+function s.spfilter(c,e,tp)
+    return (c:IsSetCard(0x344) or c:IsSetCard(0x145)) 
+        and (c:GetAttack()==0 or c:GetDefense()==0)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+    if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 then
+        --Extra Deck Lock
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+        e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+        e1:SetDescription(aux.Stringid(id,4))
+        e1:SetTargetRange(1,0)
+        e1:SetTarget(s.splimit)
+        e1:SetReset(RESET_PHASE+PHASE_END)
+        Duel.RegisterEffect(e1,tp)
+    end
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+    return not (c:IsSetCard(0x344) or c:IsSetCard(0x145)) and c:IsLocation(LOCATION_EXTRA)
+end
+function s.thfilter(c)
+    return (c:IsSetCard(0x344) or c:IsSetCard(0x145)) and c:IsFaceup() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.thfilter(chkc) end
+    if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_REMOVED,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc:IsRelateToEffect(e) then
+        Duel.SendtoHand(tc,nil,REASON_EFFECT)
+    end
+end
