972e5d87281bee0edc92d8b3abc7e5aa979c2d3c
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-22 04:01:31 +0700

    Update utility_enigma.lua

--- script/utility_enigma.lua
+++ script/utility_enigma.lua
@@ -3,7 +3,7 @@ if not Vortex then
     Vortex = {}
 end
 
-VORTEX_GLOBAL_FLAG = 111166660 
+VORTEX_GLOBAL_FLAG= 111166660 
 
 function Vortex.GetValue(c)
     if c:IsType(TYPE_LINK) then return c:GetLink() end
@@ -15,20 +15,11 @@ function Vortex.MatFilter(c,filter,tp)
     return c:IsFaceup() and c:IsAbleToDeck() and (not filter or filter(c, tp))
 end
 
--- Perbaikan Rescon agar perhitungan sum lebih stabil
-function Vortex.Rescon(total_val,recipe)
-    return function(sg,e,tp,mg)
-        local sum= 0
-        local tc= sg:GetFirst()
-        while tc do
-            sum= sum + Vortex.GetValue(tc)
-            tc= sg:GetNext()
-        end
-        if sum~= total_val then return false end
-        local res= Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())> 0
-        if not res then return false end        
-        return not recipe or recipe(sg, e, tp, mg)
-    end
+function Vortex.Rescon(sg,e,tp,mg,total_val,recipe)
+    local sum = sg:GetSum(Vortex.GetValue)
+    if sum ~= total_val then return false end
+    if Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())<=0 then return false end
+    return not recipe or recipe(sg,e,tp,mg)
 end
 
 function Vortex.AddProcedure(c,total_val,recipe)
@@ -40,36 +31,33 @@ function Vortex.AddProcedure(c,total_val,recipe)
     e1:SetRange(LOCATION_EXTRA)
     e1:SetLabel(total_val)
     e1:SetLabelObject(recipe) 
-    e1:SetCondition(Vortex.Condition(total_val,recipe))
-    e1:SetTarget(Vortex.Target(total_val,recipe))
+    e1:SetCondition(Vortex.Condition)
+    e1:SetTarget(Vortex.Target)
     e1:SetOperation(Vortex.Operation)
     e1:SetValue(99)
     c:RegisterEffect(e1)
 end
 
-function Vortex.Condition(total_val,recipe)
-    return function(e,c,tp,sg)
-        if c==nil then return true end
-        local tp=c:GetControler()
-        --Global Check Banish
-        if Duel.GetFlagEffect(tp,VORTEX_GLOBAL_FLAG)==0 then return false end      
-        local rg=Duel.GetMatchingGroup(Vortex.MatFilter,tp,LOCATION_MZONE,0,nil)
-        return aux.SelectUnselectGroup(rg,e,tp,2,99,Vortex.Rescon(total_val, recipe),0)
-    end
+function Vortex.Condition(e,c,tp,sg)
+    if c==nil then return true end
+    if Duel.GetFlagEffect(tp,VORTEX_GLOBAL_FLAG)==0 then return false end   
+    local total_val = e:GetLabel()
+    local recipe = e:GetLabelObject()
+    local rg=Duel.GetMatchingGroup(Vortex.MatFilter,tp,LOCATION_MZONE,0,nil)
+    return aux.SelectUnselectGroup(rg,e,tp,2,99,function(sg,e,tp,mg) return Vortex.Rescon(sg,e,tp,mg,total_val,recipe) end,0)
 end
 
-function Vortex.Target(total_val, recipe)
-    return function(e,tp,eg,ep,ev,re,r,rp,chk,c)
-        local rg=Duel.GetMatchingGroup(Vortex.MatFilter,tp,LOCATION_MZONE,0,nil)       
-        local cid = e:GetHandler():GetID()
-        local sg=aux.SelectUnselectGroup(rg,e,tp,2,99,Vortex.Rescon(total_val,recipe),1,tp,HINTMSG_SPSUMMON,nil,nil,true)
-        if sg and #sg > 0 then
-            sg:KeepAlive()
-            e:SetLabelObject(sg)
-            return true
-        end
-        return false
+function Vortex.Target(e,tp,eg,ep,ev,re,r,rp,chk,c)
+    local total_val = e:GetLabel()
+    local recipe = e:GetLabelObject()
+    local rg=Duel.GetMatchingGroup(Vortex.MatFilter,tp,LOCATION_MZONE,0,nil)   
+    local sg=aux.SelectUnselectGroup(rg,e,tp,2,99,function(sg,e,tp,mg) return Vortex.Rescon(sg,e,tp,mg,total_val,recipe) end,1,tp,HINTMSG_SPSUMMON,nil,nil,true)
+    if sg and #sg > 0 then
+        sg:KeepAlive()
+        e:SetLabelObject(sg)
+        return true
     end
+    return false
 end
 
 function Vortex.Operation(e,tp,eg,ep,ev,re,r,rp,c,sg)
