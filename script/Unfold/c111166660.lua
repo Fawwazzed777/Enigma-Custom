03e1b65f0263c2e1405b3a5e03dbc1edd0be65e3
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-22 04:14:57 +0700

    Update c111166660.lua

--- script/Unfold/c111166660.lua
+++ script/Unfold/c111166660.lua
@@ -1,20 +1,18 @@
---Enigmation Lord - Void Crisis
+-- Enigmation Lord - Void Crisis
 Duel.LoadScript("utility_enigma.lua")
 local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()       
-    --Summon Logic
-    --Requirement: Total Value 12, Recipe: 1 Rank 4 + 1+ Level 4 or lower
-    Vortex.AddProcedure(c,12,s.vortex_recipe)    
-    --Anti-Climbing Lock (Floodgate)
-    local e2=Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetTargetRange(1,1)
-    e2:SetTarget(s.splimit)
-    c:RegisterEffect(e2)
+    Vortex.AddProcedure(c,12,s.vortex_recipe)
+    --Anti-Climbing Lock
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(1,1)
+    e1:SetTarget(s.splimit)
+    c:RegisterEffect(e1)
     --Global Check
     if not s.global_check then
         s.global_check=true
@@ -26,34 +24,24 @@ function s.initial_effect(c)
     end
 end
 s.listed_series={0x145,0x344}
---Recipe for Void Crisis
+function s.splimit(e,c)
+    return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_XYZ)
+end
+
 function s.vortex_recipe(sg,e,tp,mg)
-    --Minimal 5 Enigmation/Phantasm di Banishment
     local g=Duel.GetMatchingGroup(function(c) 
         return (c:IsSetCard(0x145) or c:IsSetCard(0x344)) and c:IsFaceup() 
     end,tp,LOCATION_REMOVED,0,nil)
     if #g<5 then return false end
-    local g_rank4= sg:Filter(Card.IsRank,nil,4)
-    if #g_rank4~= 1 then return false end   
-    local other_mats= sg-g_rank4
-    return other_mats:All(function(c) return c:IsLevelBelow(4) and c:GetLevel() > 0 end)
+    
+    local g_rank4=sg:Filter(Card.IsRank,nil,4)
+    if #g_rank4~=1 then return false end   
+    
+    local other_mats=sg-g_rank4
+    return other_mats:All(function(c) return c:IsLevelBelow(4) and c:GetLevel()>0 end)
 end
 
---Global Check
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
     Duel.RegisterFlagEffect(0,111166660,RESET_PHASE+PHASE_END,0,1)
     Duel.RegisterFlagEffect(1,111166660,RESET_PHASE+PHASE_END,0,1)
-end
-
---Lock (Anti-Climbing)
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-    if not (c:IsLocation(LOCATION_EXTRA) or c:IsType(TYPE_EXTRA)) then return false end    
-    local tpe=c:GetType()
-    local filter_tpe= tpe&(TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ|TYPE_LINK)
-    if filter_tpe==0 then return false end 
-    return Duel.IsExistingMatchingCard(s.material_filter,sump,LOCATION_MZONE,0,1,nil,filter_tpe)
-end
-
-function s.material_filter(c,filter_tpe)
-    return c:IsType(TYPE_EXTRA) and c:IsType(filter_tpe)
 end
