c2c4995956e93e3cca4b793619a78f3dfd325b9a
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-22 01:49:03 +0700

    Update user_cards_specific_functions.lua

--- script/user_cards_specific_functions.lua
+++ script/user_cards_specific_functions.lua
@@ -3,5 +3,8 @@ function Auxiliary.DarkLightFLimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(17106529)
 end
 
+if not GenerateEffect then
+	GenerateEffect={}
 
+	end
 
