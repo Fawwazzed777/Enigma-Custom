f016d3324ab8a79d0c2ae3748f51b72c23a961d3
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-02-22 17:17:52 +0700

    F

--- script/Unfold/c111166660.lua
+++ script/Unfold/c111166660.lua
@@ -1,8 +1,6 @@
 -- Enigmation Lord - Void Crisis
-if not ENIGMA_PATCH then Duel.LoadScript("enigma_utility.lua") end
-if not VORTEX_IMPORTED then Duel.LoadScript("proc_vortex.lua") end
+Duel.LoadScript("utility_enigma.lua")
 local s,id=GetID()
-
 function s.vortex_recipe(sg,e,tp,mg)
     local g=Duel.GetMatchingGroup(function(c) 
         return (c:IsSetCard(0x145) or c:IsSetCard(0x344)) and c:IsFaceup() 
@@ -13,19 +11,11 @@ function s.vortex_recipe(sg,e,tp,mg)
     local other_mats=sg-g_rank4
     return other_mats:All(function(c) return c:IsLevelBelow(4) and c:GetLevel()>0 end)
 end
+
 function s.initial_effect(c)
     c:EnableReviveLimit()       
-    Vortex.AddProcedure(c,12,s.vortex_recipe)
-    --Anti-Climbing Lock
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetTargetRange(1,1)
-    e1:SetTarget(s.splimit)
-    c:RegisterEffect(e1)
-    --Global Check
+    Vortex.AddProcedure(c,12,s.vortex_recipe)  
+    -- Global Check Banishment
     if not s.global_check then
         s.global_check=true
         local ge1=Effect.CreateEffect(c)
@@ -41,6 +31,6 @@ function s.splimit(e,c)
 end
 
 function s.checkop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.RegisterFlagEffect(0,id,RESET_PHASE+PHASE_END,0,1)
-    Duel.RegisterFlagEffect(1,id,RESET_PHASE+PHASE_END,0,1)
+    Duel.RegisterFlagEffect(0,111166660,RESET_PHASE+PHASE_END,0,1)
+    Duel.RegisterFlagEffect(1,111166660,RESET_PHASE+PHASE_END,0,1)
 end
