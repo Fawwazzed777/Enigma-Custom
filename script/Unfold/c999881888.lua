f686135a7e71dc623a9495854c740d7a4f7977ce
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-04-02 00:17:12 +0700

    Update c999881888.lua

--- script/Unfold/c999881888.lua
+++ script/Unfold/c999881888.lua
@@ -4,6 +4,7 @@ function s.initial_effect(c)
 	--Copy Effect & Stat
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
@@ -19,27 +20,38 @@ end
 
 function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.copyfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.copyfilter,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0,TYPES_TOKEN,0,0,1,RACE_BEAST,ATTRIBUTE_EARTH)
+		and Duel.IsExistingTarget(s.copyfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,s.copyfilter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,tp,0)
 end
 
 function s.copyop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and c:IsRelateToEffect(e) and c:IsFaceup() then
+	if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end	
+	local token=Duel.CreateToken(tp,id)
+	if Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP) then
+		local c=e:GetHandler()
+		local atk=tc:GetAttack()
+		local def=tc:GetDefense()
+		local code=tc:GetOriginalCode()	
 		--Copy ATK/DEF
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetValue(tc:GetAttack())
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		c:RegisterEffect(e1)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e1)
 		local e2=e1:Clone()
 		e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
-		e2:SetValue(tc:GetDefense())
-		c:RegisterEffect(e2)		
+		e2:SetValue(def)
+		token:RegisterEffect(e2)	
 		--Copy Effects
-		c:ReplaceEffect(tc:GetOriginalCode(),RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		token:ReplaceEffect(code,RESET_EVENT+RESETS_STANDARD)		
+		Duel.SpecialSummonComplete()
 	end
 end
