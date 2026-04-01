ffaa055a2085505d7784747581c8e19e1d9e02ce
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-04-02 01:40:06 +0700

    Fix

--- script/Unfold/c999881888.lua
+++ script/Unfold/c999881888.lua
@@ -1,7 +1,7 @@
 --Vertak (Token)
 local s,id=GetID()
 function s.initial_effect(c)
-	--Copy Effect & Stat
+	--Copy Stat and Summon another "Vertak"
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
@@ -59,29 +59,34 @@ function s.copyop(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetValue(tpe)
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		token:RegisterEffect(e3)		
-		--Fail Safe (Prevent the effect from triggering if the source is itself but is already outside the field)
+		--Gain the New Quick Effect (Destruction)
 		local e4=Effect.CreateEffect(c)
-        e4:SetType(EFFECT_TYPE_SINGLE)
-        e4:SetCode(EFFECT_CANNOT_TRIGGER)
-        e4:SetRange(LOCATION_MZONE)
-        e4:SetCondition(s.trigcon)
-        token:RegisterEffect(e4)
-		local e5=Effect.CreateEffect(c)
-		e5:SetType(EFFECT_TYPE_FIELD)
-		e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e5:SetCode(EFFECT_CANNOT_ACTIVATE)
-		e5:SetRange(LOCATION_MZONE)
-		e5:SetTargetRange(1,0)
-		e5:SetValue(s.aclimit)
-		token:RegisterEffect(e5)
-		--Copy Effects
-		token:CopyEffect(code,RESET_EVENT+RESETS_STANDARD)		
+		e4:SetDescription(aux.Stringid(id,1))
+		e4:SetCategory(CATEGORY_DESTROY)
+		e4:SetType(EFFECT_TYPE_QUICK_O)
+		e4:SetCode(EVENT_FREE_CHAIN)
+		e4:SetRange(LOCATION_MZONE)
+		e4:SetCountLimit(1)
+		e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		e4:SetTarget(s.destg)
+		e4:SetOperation(s.desop)
+		e4:SetReset(RESET_EVENT+RESETS_STANDARD)
+		token:RegisterEffect(e4)		
 		Duel.SpecialSummonComplete()
 	end
 end
-function s.trigcon(e)
-    return not e:GetHandler():IsLocation(LOCATION_MZONE)
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc~=e:GetHandler() end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g:AddCard(e:GetHandler()),2,0,0)
 end
-function s.aclimit(e,re,tp)
-	return re:GetHandler()==e:GetHandler() and not re:GetHandler():IsLocation(LOCATION_ONFIELD)
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+		Duel.Destroy(c,REASON_EFFECT)
+	end
 end
