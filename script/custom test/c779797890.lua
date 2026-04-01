ae11bb1d1b781b2b10b6d7084fa3935479fa3cfa
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-04-01 17:23:14 +0700

    Update c779797890.lua

--- script/custom test/c779797890.lua
+++ script/custom test/c779797890.lua
@@ -3,8 +3,9 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion Material
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,s.matfilter,3,s.gcheck)
-	s.material_filter=function(mc,mg) return mg:IsExists(s.matfilter,1,mc) and not mg:IsExists(Card.IsRace,1,mc,mc:GetRace())end
+	Fusion.AddProcMix(c,true,true,s.matfilter,s.matfilter,s.matfilter)
+	s.material_filter=function(mc,mg)
+	return mg:GetClassCount(Card.GetRace)==#mg end
 	--Banish on Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -43,9 +44,6 @@ s.listed_series={0x145,0x344}
 s.material_setcode={0x145,0x344}
 function s.matfilter(c,fc,sumtype,tp)
     return (c:IsSetCard(0x344) or c:IsSetCard(0x145) or c:IsSummonType(SUMMON_TYPE_SPECIAL))
-end
-function s.gcheck(g)
-    return g:GetClassCount(Card.GetRace)==#g
 end
 function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
