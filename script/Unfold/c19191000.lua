06c95d646eee389725f8dde5389951bafbcd8bb7
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-04-04 07:45:03 +0700

    Fix

--- script/Unfold/c19191000.lua
+++ script/Unfold/c19191000.lua
@@ -32,31 +32,30 @@ function s.exfilter2(c,e,tp)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if not Duel.IsPlayerCanDiscardDeck(tp,1) then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
 	local ct=Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)
 	if ct==0 then return end
-	if ct>3 then ct=3 end
+	local max_num=math.min(ct,3,ft)	
 	local t={}
-	for i=1,ct do t[i]=i end
+	for i=1,max_num do t[i]=i end	
 	Duel.Hint(HINTMSG_NUMBER,tp,HINT_NUMBER)
-	local ac=Duel.AnnounceNumber(tp,table.unpack(t))
+	local ac=Duel.AnnounceNumber(tp,table.unpack(t))	
 	Duel.ConfirmDecktop(tp,ac)
 	local g=Duel.GetDecktopGroup(tp,ac)
-	local sg=g:Filter(s.exfilter2,nil)
-	if #sg>0 then
+	if #g>0 then
 		Duel.DisableShuffleCheck()
-		local xg=Duel.SendtoGrave(sg,REASON_EFFECT+REASON_REVEAL)
-		local rg=Duel.SelectMatchingCard(tp,s.exfilter1,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,xg,nil,e,tp)
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		if ft<=0 then return end
-		if ft>3 then ft=3 end
-		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-		if #rg>0 and ft then 
-		Duel.SpecialSummon(rg,0,tp,tp,false,false,POS_FACEUP)
+		local sg=g:Filter(s.exfilter2,nil,e,tp)
+		if #sg>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local spg=sg:Select(tp,1,ft,nil) 
+			if #spg>0 then
+				Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+		local rem=g:Filter(function(c) return not spg or not spg:IsContains(c) end,nil)
+		if #rem>0 then
+			Duel.SendtoGrave(rem,REASON_EFFECT+REASON_REVEAL)
+		end
 	end
-	ac=ac-#sg
-	if ac>0 then
-		Duel.MoveToDeckBottom(ac,tp)
-		Duel.SortDeckbottom(tp,tp,ac)
-end
-end
 end
