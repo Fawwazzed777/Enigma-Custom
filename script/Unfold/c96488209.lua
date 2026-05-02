50f56b744cd30af55812039c647ebabea91784e4
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-05-03 01:58:03 +0700

    Buff

--- script/Unfold/c96488209.lua
+++ script/Unfold/c96488209.lua
@@ -30,13 +30,16 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 	--Would Leave the Field
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetCode(EFFECT_SEND_REPLACE)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_DISABLE+CATEGORY_DESTROY)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e4:SetCondition(s.effcon)
-	e4:SetTarget(s.reptg)
+	e4:SetTarget(s.dtg)
+	e4:SetOperation(s.dop)
 	c:RegisterEffect(e4)
 	--indes
 	local en=Effect.CreateEffect(c)
@@ -80,7 +83,37 @@ function s.valcheck(e,c)
 	end
 end
 function s.effcon(e)
-	return e:GetHandler():GetFlagEffect(id)>0
+	if not e:GetHandler():GetFlagEffect(id)>0 or 
+	e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) or not Duel.IsChainNegatable(ev) then return false end
+	if re:IsHasCategory(CATEGORY_NEGATE)
+		and Duel.GetChainInfo(ev-1,CHAININFO_TRIGGERING_EFFECT):IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
+	if not Duel.IsChainDisablable(ev) then return false end
+	local eb,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_REMOVE)
+	if eb and tg and tg:IsExists(Card.IsOnField,1,nil) then return true end
+	local eb,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	if eb and tg and tg:IsExists(Card.IsOnField,1,nil) then return true end
+	local eb,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_TODECK)
+	if eb and tg and tg:IsExists(Card.IsOnField,1,nil) then return true end
+	local eb,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_TOGRAVE)
+	if eb and tg and tg:IsExists(Card.IsOnField,1,nil) then return true end
+	local eb,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_TOHAND)
+	if eb and tg and tg:IsExists(Card.IsOnField,1,nil) then return true end
+	return eb and tg and tg:IsExists(Card.IsOnField,1,nil)
+end
+--
+function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_CONTROL,eg,1,0,0)
+end
+end
+function s.dop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.NegateEffect(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.BreakEffect()
+		Duel.Overlay(c,eg,true)
+end
 end
 --
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -107,26 +140,4 @@ function s.rop(e,tp,eg,ep,ev,re,r,rp)
 end
 end
 end
---
-function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE)
-	 and c:IsFaceup() and c:GetOverlayGroup():IsExists(Card.IsAbleToRemoveAsCost,1,nil) end
-	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-	local ovg=c:GetOverlayGroup()
-	local rg=Duel.Remove(ovg,POS_FACEUP,REASON_COST)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,rg,rg,nil)
-		if rg then
-		Duel.HintSelection(g)
-		if #g==0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)==0 then return end
-		local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_REMOVED)
-		if #og>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
-			local sg=og:Select(tp,1,2,nil)
-			Duel.BreakEffect()
-			Duel.Overlay(c,sg)
-	end
-		return true
-	else return false end
-end
-end
+
