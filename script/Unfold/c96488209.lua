f886f6f29650d99895d41af5575ec670e638e728
Fawwazzed777 <115336509+Fawwazzed777@users.noreply.github.com>
2026-05-03 03:40:53 +0700

    F

--- script/Unfold/c96488209.lua
+++ script/Unfold/c96488209.lua
@@ -4,40 +4,28 @@ function s.initial_effect(c)
 	--xyz summon
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),8,5)
 	c:EnableReviveLimit()
-	--Check materials used for its Xyz summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.regcon)
-	e1:SetOperation(s.regop)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_MATERIAL_CHECK)
-	e2:SetValue(s.valcheck)
-	e2:SetLabelObject(e1)
-	c:RegisterEffect(e2)
 	--ATK Up
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetCost(s.cost)
-	e3:SetTarget(s.rtg)
-	e3:SetOperation(s.rop)
-	c:RegisterEffect(e3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.rtg)
+	e1:SetOperation(s.rop)
+	c:RegisterEffect(e1)
 	--Would Leave the Field
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e4:SetCode(EFFECT_SEND_REPLACE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1)
-	e4:SetCondition(s.effcon)
-	e4:SetTarget(s.reptg)
-	c:RegisterEffect(e4)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DISABLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCondition(s.effcon)
+	e2:SetTarget(s.dtg)
+	e2:SetOperation(s.dop)
+	c:RegisterEffect(e2)
 	--indes
 	local en=Effect.CreateEffect(c)
 	en:SetType(EFFECT_TYPE_SINGLE)
@@ -55,32 +43,39 @@ function s.initial_effect(c)
 	c:RegisterEffect(et)
 end
 s.listed_names={96488201}
-
 function s.splimit(e,se,sp,st)
 	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or aux.xyzlimit(e,se,sp,st)
 end
---Check
-function s.regcon(e,tp,eg,ep,ev,re,r,rp)
-	return (e:GetHandler():GetSummonType()&SUMMON_TYPE_XYZ)==SUMMON_TYPE_XYZ and e:GetLabel()==1
-end
-function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
-	c:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,0))
-end
-function s.matfilter(c,sc)
-	return c:IsCode(96488201)
-end
-function s.valcheck(e,c)
-	local g=c:GetMaterial()
-	if g:IsExists(s.matfilter,1,nil,c) then
-		e:GetLabelObject():SetLabel(1)
-	else
-		e:GetLabelObject():SetLabel(0)
-	end
-end
 function s.effcon(e)
-	return e:GetHandler():GetFlagEffect(id)>0
+	local c=e:GetHandler()
+	local has_mat=c:GetOverlayGroup():IsExists(Card.IsCode,1,nil,96488201)
+	if not has_mat or not Duel.IsChainNegatable(ev) then return false end
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_RELEASE)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_REMOVE)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_TOHAND)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0	
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_TODECK)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0	
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_TOGRAVE)
+	return ex and tg~=nil and tc+tg:FilterCount(Card.IsOnField,nil)-#tg>0
+end
+--
+function s.dtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.dop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateActivation(ev)
+	local c=e:GetHandler()
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.BreakEffect()
+		Duel.Overlay(c,eg,true)
+	end
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
 end
 --
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -107,26 +102,4 @@ function s.rop(e,tp,eg,ep,ev,re,r,rp)
 end
 end
 end
---
-function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return not c:IsReason(REASON_REPLACE)
-	 and c:IsFaceup() and c:GetOverlayGroup():IsExists(Card.IsAbleToRemoveAsCost,1,nil) end
-	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-	local ovg=c:GetOverlayGroup()
-	local rg=Duel.Remove(ovg,POS_FACEUP,REASON_COST)
-	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,rg,rg,nil)
-		if rg then
-		Duel.HintSelection(g)
-		if #g==0 and Duel.Remove(g,POS_FACEUP,REASON_EFFECT)==0 then return end
-		local og=Duel.GetOperatedGroup():Filter(Card.IsLocation,nil,LOCATION_REMOVED)
-		if #og>0 then
-			Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,2))
-			local sg=og:Select(tp,1,2,nil)
-			Duel.BreakEffect()
-			Duel.Overlay(c,sg)
-	end
-		return true
-	else return false end
-end
-end
+
