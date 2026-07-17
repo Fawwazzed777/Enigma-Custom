9da67dc90b64205f57bc2f8fa1a18c0407811dd1
Fawwazzed777 <fawwazzed@gmail.com>
2026-07-17 21:42:18 +0700

    Enigmation Lord - Chaos Spectre Dragon

---
+++ script/custom test/c987601000.lua
@@ -0,0 +1,117 @@
+--Enigmation - Chaos Spectre Dragon 
+local s,id=GetID()
+function s.initial_effect(c)
+	Xyz.AddProcedure(c,s.xyzfill,nil,2,nil,nil,nil,nil,false,s.xyzcheck)
+	c:EnableReviveLimit()
+	--Banish 1 card (Xyz Summon)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCost(Cost.SoftOncePerChain(id))
+	e1:SetCondition(s.bancon)
+	e1:SetTarget(s.bantg)
+	e1:SetOperation(s.banop)
+	c:RegisterEffect(e1)
+	--Banish 1 card on chain
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.bancon2)
+	e2:SetCost(Cost.SoftOncePerChain(id))
+	e2:SetTarget(s.bantg)
+	e2:SetOperation(s.banop)
+	c:RegisterEffect(e2)
+	e1:SetCountLimit(2,id)
+	e2:SetCountLimit(2,id)
+	--Debuff 1600 ATK/DEF (Quick Effect)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE+CATEGORY_TOGRAVE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+	e3:SetCost(Cost.DetachFromSelf(1))
+	e3:SetTarget(s.atktg)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+	--Unaffected by monster effect while it has no material, except "Enigmation" or "Imaginary Force" monster.
+	local ei=Effect.CreateEffect(c)
+	ei:SetType(EFFECT_TYPE_SINGLE)
+	ei:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	ei:SetRange(LOCATION_MZONE)
+	ei:SetCode(EFFECT_IMMUNE_EFFECT)
+	ei:SetValue(s.efilter)
+	c:RegisterEffect(ei)
+end
+function s.xyzfill(c,xyz,sumtype,tp)
+	return c:IsType(TYPE_XYZ,xyz,sumtype,tp) and c:IsRank(4)
+end
+function s.xyzfilter(c,xyz,tp)
+	return c:IsSetCard(0x344,xyz,SUMMON_TYPE_XYZ,tp) or c:IsSetCard(0x303,xyz,SUMMON_TYPE_XYZ,tp)
+end
+function s.xyzcheck(g,tp,xyz)
+	local mg=g:Filter(function(c) return not c:IsHasEffect(EFFECT_EQUIP_SPELL_XYZ_MAT) end,nil)
+	return mg:IsExists(s.xyzfilter,1,nil,xyz,tp)
+end
+function s.bancon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.bancon2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	return rp~=tp
+end
+function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,0,0)
+end
+function s.banop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,c)
+	if c:IsRelateToEffect(e) then
+		Duel.BreakEffect()
+		local sg=g:Select(tp,1,1,c)
+		Duel.HintSelection(sg)
+		Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+	end
+end
+function s.debuff(c,e)
+	return c:IsFaceup() and (c:HasNonZeroAttack() or c:HasNonZeroDefense())
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.desfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.debuff,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.debuff,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,c)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		--Decrease ATK/DEF
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.efilter(e,te)
+	local c=e:GetHandler()
+	return c:GetOverlayCount()==0 and te:IsMonsterEffect() and te:GetOwner()~=e:GetOwner() and not (te:IsCardSetcode(0x344) or te:IsCardSetcode(0x303))
+end
