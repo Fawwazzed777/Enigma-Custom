4d0d43379228af2a21e603624131e69abe57263c
Fawwazzed777 <fawwazzed@gmail.com>
2026-07-27 19:14:18 +0700

    Fix

--- script/user_cards_specific_functions.lua
+++ script/user_cards_specific_functions.lua
@@ -1,10 +1,9 @@
 --Special Summon limit for "Darklight Fusion"-related Fusion monsters
 function Auxiliary.DarkLightFLimit(e,se,sp,st)
 	if not e:GetHandler():IsLocation(LOCATION_EXTRA) then return true end
-	if not se then return false end
+	if not se then return true end
 	local sc=se:GetHandler()
-	return sc:IsCode(17106529) or se:IsHasProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE) 
-		or sc:IsCode(899982111)--Chimera Dragon Erva
+	return sc:IsCode(17106529) or sc:IsCode(899982111) or se:IsHasProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 end
 
 if not GenerateEffect then
