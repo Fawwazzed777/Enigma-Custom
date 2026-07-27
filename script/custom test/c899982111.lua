094cf530ba92b76e00d3122b1842cf6f862c3714
Fawwazzed777 <fawwazzed@gmail.com>
2026-07-27 19:03:11 +0700

    Chimera Dragon Erva

---
+++ script/custom test/c899982111.lua
@@ -0,0 +1,102 @@
+--Chimera Dragon Erva
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Summon & Destroy OR Search
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY+CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)	
+	--Fusion Summon (Treated as Darklight Fusion)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.fuscon)
+	e2:SetCost(s.fuscost)
+	e2:SetTarget(s.fustg)
+	e2:SetOperation(s.fusop)
+	c:RegisterEffect(e2)
+end
+s.listed_names={17106529}
+function s.spconfilter(c)
+	return not c:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+	return #g==0 or (#g>0 and not g:IsExists(s.spconfilter,1,nil))
+end
+function s.thfilter(c)
+	return c:IsCode(17106529) and c:IsAbleToHand()
+end
+function s.desfilter(c)
+	return c:IsFaceup() and c:IsSummonType(SUMMON_TYPE_SPECIAL)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		local b1=Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
+		local b2=Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil)		
+		if not (b1 or b2) then return end	
+		local op=0
+		if b1 and b2 then
+			op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+		elseif b1 then
+			op=0
+		else
+			op=1
+		end	
+		Duel.BreakEffect()
+		if op==0 then
+			--Destroy 1 Special Summoned monster
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,c)
+			if #g>0 then
+				Duel.HintSelection(g)
+				Duel.Destroy(g,REASON_EFFECT)
+			end
+		else
+			--Add 1 Darklight Fusion
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
+			if #g>0 then
+				Duel.SendtoHand(g,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,g)
+			end
+		end
+	end
+end
+
+function s.fuscon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
+end
+function s.fuscost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1500) end
+	Duel.PayLPCost(tp,1500)
+end
+function s.fustg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local params={chkf=tp, mg=Duel.GetFusionMaterial(tp)}
+		return Fusion.SummonEffTG(params)(e,tp,eg,ep,ev,re,r,rp,chk)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.fusop(e,tp,eg,ep,ev,re,r,rp)
+	local params={chkf=tp, mg=Duel.GetFusionMaterial(tp)}
+	Fusion.SummonEffOP(params)(e,tp,eg,ep,ev,re,r,rp)
+end
--- script/user_cards_specific_functions.lua
+++ script/user_cards_specific_functions.lua
@@ -1,6 +1,10 @@
 --Special Summon limit for "Darklight Fusion"-related Fusion monsters
 function Auxiliary.DarkLightFLimit(e,se,sp,st)
-	return not e:GetHandler():IsLocation(LOCATION_EXTRA) or se:GetHandler():IsCode(17106529)
+	if not e:GetHandler():IsLocation(LOCATION_EXTRA) then return true end
+	if not se then return false end
+	local sc=se:GetHandler()
+	return sc:IsCode(17106529) or se:IsHasProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE) 
+		or sc:IsCode(899982111)--Chimera Dragon Erva
 end
 
 if not GenerateEffect then
