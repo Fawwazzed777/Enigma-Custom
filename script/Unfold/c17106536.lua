8f63a7e16fffae8504a06398ff22ced49ad0bc26
Fawwazzed777 <fawwazzed@gmail.com>
2026-07-27 21:34:41 +0700

    Fix

--- script/Unfold/c17106536.lua
+++ script/Unfold/c17106536.lua
@@ -48,7 +48,6 @@ end
 function s.ffilter1(c,fc,sumtype,tp)
 	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ)
 end
-
 function s.copycon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
@@ -85,39 +84,8 @@ end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()>0
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
-	--Helper
-	local function valid_chain(ch_index)
-		local te=Duel.GetChainInfo(ch_index,CHAININFO_TRIGGERING_EFFECT)
-		if not te or not te:IsActiveType(TYPE_MONSTER) or not Duel.IsChainNegatable(ch_index) then 
-			return false 
-		end
-		local tc=te:GetHandler()
-		return tc~=c and te:GetHandler()~=c
-	end
-	if chk==0 then
-		for i=1,ev do
-			if valid_chain(i) then return true end
-		end
-		return false
-	end
-	local t={}
-	for i=1,ev do
-		if valid_chain(i) then
-			table.insert(t,i)
-		end
-	end
-	--Choose which Chain Link you want to negate and copy
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local sel=Duel.AnnounceNumber(tp,table.unpack(t))
-	e:SetLabel(sel)	
-	local te=Duel.GetChainInfo(sel,CHAININFO_TRIGGERING_EFFECT)
-	Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,0,0)
-end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	--Helper to store chain link (Because you can't legally target monster in hand, plus i may need to ask judge for this)
 	local function valid_chain(ch_index)
 		local te=Duel.GetChainInfo(ch_index,CHAININFO_TRIGGERING_EFFECT)
 		if not te or not te:IsActiveType(TYPE_MONSTER) or not Duel.IsChainNegatable(ch_index) then 
@@ -138,49 +106,54 @@ function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 			table.insert(t,i)
 		end
 	end
-
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local sel=Duel.AnnounceNumber(tp,table.unpack(t))
 	e:SetLabel(sel)	
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,nil,1,0,0)
 end
-
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local sel_ev=e:GetLabel()
 	if sel_ev<=0 then return end	
 	local te=Duel.GetChainInfo(sel_ev,CHAININFO_TRIGGERING_EFFECT)
-	if not te then return end	
+	if not te then return end		
 	--NEGATE ACTIVATION ON CHAIN
 	if Duel.NegateActivation(sel_ev) then	
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
-			Duel.BreakEffect()		
+			Duel.BreakEffect()				
+			local stolen_e=te:Clone()
 			local copied_tg=te:GetTarget()
-			local copied_op=te:GetOperation()	
-			--STOLEN EFFECT
+			local copied_op=te:GetOperation()			
+			--STOLEN EFFECT REGISTER TO DARK TEMPLAR
 			local e1=Effect.CreateEffect(c)
-			e1:SetDescription(aux.Stringid(id,2))--"Execute Stolen Monster Effect"
+			e1:SetDescription(aux.Stringid(id,2))
 			e1:SetType(EFFECT_TYPE_QUICK_O)
 			e1:SetCode(EVENT_FREE_CHAIN)
 			e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 			e1:SetRange(LOCATION_MZONE)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e1:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)			
+			e1:SetTarget(function(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+				if chkc then
+					if copied_tg then
+						return copied_tg(stolen_e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+					end
+					return false
+				end
 				if chk==0 then
 					if copied_tg then
-						return copied_tg(e,tp,eg,ep,ev,re,r,rp,0)
+						return copied_tg(stolen_e,tp,eg,ep,ev,re,r,rp,0)
 					end
 					return true
 				end
 				if copied_tg then
-					copied_tg(e,tp,eg,ep,ev,re,r,rp,1)
-				end
-			end)
-			e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
-				if copied_op then
-					copied_op(e,tp,eg,ep,ev,re,r,rp)
+					copied_tg(stolen_e,tp,eg,ep,ev,re,r,rp,1)
 				end
 			end)		
+			e1:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+				if copied_op then
+					copied_op(stolen_e,tp,eg,ep,ev,re,r,rp)
+				end
+			end)					
 			c:RegisterEffect(e1)
 			Duel.Hint(HINT_CARD,0,te:GetHandler():GetOriginalCode())
 		end
