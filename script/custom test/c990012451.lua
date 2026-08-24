50f7f66f6b89eef20dc18f45b894a3d5aca1b7cc
Fawwazzed777 <fawwazzed@gmail.com>
2026-08-24 09:06:35 +0700

    990012451

---
+++ script/custom test/c990012451.lua
@@ -0,0 +1,126 @@
+--Enigmation Force - Light Wing Layra
+local s,id=GetID()
+function s.initial_effect(c)
+	--Tribute 1 DARK & 1 LIGHT (as effect) Special Summon itself
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Tribute self (as effect) to SS Dark Wing Layra
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.swcon)
+	e2:SetTarget(s.swtg)
+	e2:SetOperation(s.swop)
+	c:RegisterEffect(e2)
+	--Add 1 monster from GY/Banishment to hand & gain ATK
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={990012450}
+function s.relfilter(c,att)
+	return c:IsAttribute(att) and c:IsReleasableByEffect()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then 
+		local g1=Duel.GetMatchingGroup(s.relfilter,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_DARK)
+		local g2=Duel.GetMatchingGroup(s.relfilter,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_LIGHT)
+		return #g1>0 and #g2>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>-2 
+			and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g1=Duel.GetMatchingGroup(s.relfilter,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_DARK)
+	local g2=Duel.GetMatchingGroup(s.relfilter,tp,LOCATION_MZONE,0,nil,ATTRIBUTE_LIGHT)
+	if #g1==0 or #g2==0 then return end	
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local sg1=g1:Select(tp,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local sg2=g2:Select(tp,1,1,sg1)
+	sg1:Merge(sg2)	
+	if #sg1==2 and Duel.Release(sg1,REASON_EFFECT)>0 then
+		Duel.BreakEffect()
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.swcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
+end
+function s.spfilter2(c,e,tp)
+	return c:IsCode(990012450) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED))
+end
+function s.swtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then 
+		return c:IsReleasableByEffect() 
+			and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.swop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or not c:IsReleasableByEffect() then return end 
+	if Duel.Release(c,REASON_EFFECT)>0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	if not re then return false end
+	local rc=re:GetHandler()
+	return rc and rc:IsAttribute(ATTRIBUTE_DARK) and re:IsActiveType(TYPE_MONSTER)
+end
+function s.thfilter(c)
+	return c:IsMonster() and c:IsAbleToHand() and (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_HAND) then
+		Duel.ConfirmCards(1-tp,g)
+		if c:IsRelateToEffect(e) and c:IsFaceup() and c:HasLevel() then
+			Duel.BreakEffect()
+			local atk=c:GetLevel()*200
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(atk)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+			c:RegisterEffect(e1)
+		end
+	end
+end
