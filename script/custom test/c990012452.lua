4e5eda9f0347bcdb2d4d939ea45aa7edf7129211
Fawwazzed777 <fawwazzed@gmail.com>
2026-08-26 04:33:36 +0700

    F

---
+++ script/custom test/c990012452.lua
@@ -0,0 +1,107 @@
+--Enigmation Force - Darklord Layra
+local s,id=GetID()
+local SET_LAYRA=0x1840
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,s.sfilter,s.ffilter)
+    --This card is also a DARK Attribute while on the field or GY (This effect cannot be negated).
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_ADD_ATTRIBUTE)
+    e1:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+    e1:SetValue(ATTRIBUTE_DARK)
+    c:RegisterEffect(e1)
+    --Tribute 1 monster; Special Summon "Layra" with different Attribute, then Banish 1 card.
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,id)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+    e2:SetCost(s.spcost)
+    e2:SetTarget(s.sptg)
+    e2:SetOperation(s.spop)
+    c:RegisterEffect(e2)
+    --If this card battles, during the Damage Step: Banish 1 monster; gains ATK= Level/Rank x300.
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_BATTLE_START)
+    e3:SetCountLimit(1,{id,1})
+    e3:SetCondition(s.atkcon)
+    e3:SetCost(s.atkcost)
+    e3:SetOperation(s.atkop)
+    c:RegisterEffect(e3)
+end
+s.listed_series={SET_LAYRA}
+function s.sfilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_FAIRY,fc,sumtype,tp) and c:IsLevel(10) and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_FAIRY,fc,sumtype,tp) and c:IsLevel(10) and c:IsAttribute(ATTRIBUTE_LIGHT)
+end
+function s.spfilter(c,e,tp,attr)
+    return c:IsSetCard(SET_LAYRA) and not c:IsAttribute(attr)
+        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.cfilter(c,e,tp)
+    return Duel.GetMZoneCount(tp,c)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp,c:GetAttribute())
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,e,tp) end
+    local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,e,tp)
+    e:SetLabel(g:GetFirst():GetAttribute())
+    Duel.Release(g,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED)
+    Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_ONFIELD)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local attr=e:GetLabel()
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp,attr)
+    if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+        local rg=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,nil)
+        if #rg>0 then
+            Duel.BreakEffect()
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+            local sg=rg:Select(tp,1,1,nil)
+            Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+        end
+    end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+    return e:GetHandler():GetBattleTarget()~=nil
+end
+function s.rmfilter(c)
+    return c:IsMonster() and (c:HasLevel() or c:HasRank()) and c:IsAbleToRemoveAsCost()
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+    local tc=g:GetFirst()
+    local val=tc:GetLevel()
+    if tc:IsType(TYPE_XYZ) then val=tc:GetRank() end
+    e:SetLabel(val)
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    if c:IsRelateToEffect(e) and c:IsFaceup() then
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_UPDATE_ATTACK)
+        e1:SetValue(e:GetLabel()*300)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+        c:RegisterEffect(e1)
+    end
+end
