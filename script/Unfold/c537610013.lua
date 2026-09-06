83c02f1e1f7af9877901a042c61b6bfc151d4057
Fawwazzed777 <fawwazzed@gmail.com>
2026-09-07 00:46:10 +0700

    Add support for Dichroic

---
+++ script/Unfold/c537610013.lua
@@ -0,0 +1,93 @@
+--Dichroic Blader Rein
+--Scripted by fawwazzed
+local s,id=GetID()
+function s.initial_effect(c)
+	--You can reveal 1 other "Dichroic" card in your hand; Special Summon this card from your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCost(Cost.Reveal(function(c) return c:IsSetCard(0x391) and not c:IsPublic() end,true))
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Add 1 "Polymerization" or "Fusion Deployment"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+	--Add this card if you Special Summon a WIND Fusion Monster
+	local e2a=Effect.CreateEffect(c)
+	e2a:SetDescription(aux.Stringid(id,2))
+	e2a:SetCategory(CATEGORY_TOHAND)
+	e2a:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2a:SetProperty(EFFECT_FLAG_DELAY,EFFECT_FLAG2_CHECK_SIMULTANEOUS)
+	e2a:SetCode(EVENT_SUMMON_SUCCESS)
+	e2a:SetRange(LOCATION_GRAVE)
+	e2a:SetCountLimit(1,{id,2})
+	e2a:SetCondition(s.thcon1)
+	e2a:SetTarget(s.thtg1)
+	e2a:SetOperation(s.thop1)
+	c:RegisterEffect(e2a)
+	local e2b=e2a:Clone()
+	e2b:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2b)
+end
+s.listed_series={0x391}
+s.listed_names={CARD_POLYMERIZATION,6498706}
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		c:UpdateAttack(300,RESET_EVENT|RESETS_STANDARD)
+	end
+end
+function s.thfilter(c)
+	return (c:IsCode(CARD_POLYMERIZATION) or c:IsCode(6498706)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.thcon1filter(c,tp)
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsType(TYPE_FUSION) and c:IsControler(tp) and c:IsFaceup()
+end
+function s.thcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.thcon1filter,1,nil,tp)
+end
+function s.thtg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.thop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SendtoHand(c,nil,REASON_EFFECT)>0 and c:IsLocation(LOCATION_HAND) then
+		Duel.ShuffleHand(tp)
+	end
+end
